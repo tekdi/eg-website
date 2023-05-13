890db71d49d45bc2e3a1dae388a9a9b5327c6255
Sagar Takle <sagarkoshti1990@gmail.com>
2023-05-13 23:14:04 +0530

    fix: login page

--- lib/common-lib/src/components/theme.ts
+++ lib/common-lib/src/components/theme.ts
@@ -137,7 +137,7 @@ const DEFAULT_THEME = {
         }),
         redOutlineBtn: () => ({
           bg: `#FFFFFF`,
-          borderWidth: '2',
+          borderWidth: '1',
           borderColor: `#790000`,
           _hover: {
             bg: `#FFFFFF`
@@ -146,10 +146,9 @@ const DEFAULT_THEME = {
             bg: `#f9f5f5`
           },
           _text: {
-            fontWeight: '600',
             color: '#790000'
           },
-          rounded: '4'
+          rounded: '4px'
         })
       }
     }
@@ -334,6 +333,9 @@ const DEFAULT_THEME = {
     },
     progressBarColor: {
       200: '#10B981'
+    },
+    bgGreyColor: {
+      200: '#F4F4F7'
     },
     textGreyColor: {
       100: '#616161',
