15651492dfefa2319f7fb865e3ca3179de18a67a
dhanashree <dhanashree.patil@tekditechnologies.com>
2023-05-17 19:04:38 +0530

    Fix:[Deleted unnecessary files]

--- apps/front-end/src/routes/guestRoutes.js
+++ apps/front-end/src/routes/guestRoutes.js
@@ -2,8 +2,6 @@ import NotFound from "pages/NotFound";
 import React from "react";
 import Home from "../pages/front-end/Home";
 const LoginComponent = React.lazy(() => import("auth/Login"));
-import MyProfile from "../pages/front-end/MyProfile"
-import MyProfileDashboard from "pages/front-end/MyProfileDashboard";
 
 import PrerakProfileDetailsView from "pages/front-end/PrerakProfileDetailsView";
 import Orientation from "pages/front-end/Orientation";
@@ -21,11 +19,8 @@ export default [
     path: "*",
     component: LoginComponent,
   },
-  { path: "/myprofile", component: MyProfile },
-  { path: "/myprofile/myprofiledashboard", component: MyProfileDashboard },
-  
+
   { path: "/prerakprofiledetails", component: PrerakProfileDetailsView },
-  { path: "/orientation", component: Orientation},
-  { path: "/agprofile", component: AgLearnerProfileView},
-  
+  { path: "/orientation", component: Orientation },
+  { path: "/agprofile", component: AgLearnerProfileView },
 ];
