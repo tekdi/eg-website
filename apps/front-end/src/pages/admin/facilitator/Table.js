f6539e21b4a81ec4066f066ca8d9d6632e323785
Nikhil <nikhil_bhat@gamil.com>
2023-05-30 09:35:21 +0530

    duplicates imports removed

--- apps/front-end/src/pages/admin/facilitator/Table.js
+++ apps/front-end/src/pages/admin/facilitator/Table.js
@@ -20,16 +20,7 @@ import {
   Image,
   Box,
 } from "native-base";
-import {
-  Button,
-  HStack,
-  Input,
-  Text,
-  VStack,
-  Modal,
-  Image,
-  Box,
-} from "native-base";
+
 import React from "react";
 import DataTable from "react-data-table-component";
 import { useNavigate } from "react-router-dom";
