5e6f0a81ea96c94fa69a53bff170cf121ea8034f
dhanashree <dhanashree.patil@tekditechnologies.com>
2023-06-16 13:41:13 +0530

    Fix[removed react fragment ]

--- apps/front-end/src/pages/front-end/BenificiaryEnrollment.js
+++ apps/front-end/src/pages/front-end/BenificiaryEnrollment.js
@@ -44,175 +44,184 @@ export default function BenificiaryEnrollment() {
 
   return (
     <Layout _appBar={{ name: t("ENROLLMENT_DETAILS") }}>
-      <VStack bg="bgGreyColor.200">
-        <VStack px="5" pt="3">
-          <VStack
-            px="5"
-            py="4"
-            mb="3"
-            borderRadius="10px"
-            borderWidth="1px"
-            bg="white"
-            borderColor="appliedColor"
-          >
-            <HStack justifyContent="space-between" alignItems="Center">
-              <FrontEndTypo.H3 bold color="textGreyColor.800">
-                {t("ENROLLMENT_DETAILS")}
+      <VStack bg="bgGreyColor.200" px="5" pt="3">
+        <VStack
+          px="5"
+          py="4"
+          mb="3"
+          borderRadius="10px"
+          borderWidth="1px"
+          bg="white"
+          borderColor="appliedColor"
+        >
+          <HStack justifyContent="space-between" alignItems="Center">
+            <FrontEndTypo.H3 bold color="textGreyColor.800">
+              {t("ENROLLMENT_DETAILS")}
+            </FrontEndTypo.H3>
+            <IconByName
+              name="EditBoxLineIcon"
+              color="iconColor.100"
+              _icon={{ size: "20" }}
+              onPress={(e) => {
+                navigate(`/beneficiary/edit/${id}/enrollment-details`);
+              }}
+            />
+          </HStack>
+          <Box>
+            <Progress
+              value={arrList(benificiary?.program_beneficiaries, [
+                "type_of_enrollement",
+                "enrollment_status",
+                "enrolled_for_board",
+                "enrollment_number",
+                "subjects",
+              ])}
+              size="xs"
+              colorScheme="info"
+            />
+          </Box>
+          <VStack space="2" pt="5">
+            <HStack
+              alignItems="Center"
+              justifyContent="space-between"
+              borderBottomWidth="1px"
+              borderBottomColor="appliedColor"
+            >
+              <FrontEndTypo.H3 color="textGreyColor.50" flex="0.3" pb="2">
+                {t("TYPE_OF_ENROLLMENT")}
               </FrontEndTypo.H3>
-              <IconByName
-                name="EditBoxLineIcon"
-                color="iconColor.100"
-                _icon={{ size: "20" }}
-                onPress={(e) => {
-                  navigate(`/beneficiary/edit/${id}/enrollment-details`);
-                }}
-              />
+
+              <FrontEndTypo.H3 color="textGreyColor.800" flex="0.4">
+                {benificiary?.program_beneficiaries?.type_of_enrollement
+                  ? benificiary?.program_beneficiaries?.type_of_enrollement
+                  : "-"}
+              </FrontEndTypo.H3>
             </HStack>
+
+            <HStack
+              alignItems="Center"
+              justifyContent="space-between"
+              borderBottomWidth="1px"
+              borderBottomColor="appliedColor"
+            >
+              <FrontEndTypo.H3 color="textGreyColor.50" flex="0.3" pb="2">
+                {t("ENROLLMENT_STATUS")}
+              </FrontEndTypo.H3>
+
+              <FrontEndTypo.H3 color="textGreyColor.800" flex="0.4">
+                {benificiary?.program_beneficiaries?.enrollment_status
+                  ? t(
+                      benificiary?.program_beneficiaries?.enrollment_status.toUpperCase()
+                    )
+                  : "-"}
+              </FrontEndTypo.H3>
+            </HStack>
+
+            <HStack
+              alignItems="Center"
+              justifyContent="space-between"
+              borderBottomWidth="1px"
+              borderBottomColor="appliedColor"
+            >
+              <FrontEndTypo.H3 color="textGreyColor.50" flex="0.3" pb="2">
+                {t("ENROLLMENT_BOARD")}
+              </FrontEndTypo.H3>
+
+              <FrontEndTypo.H3 color="textGreyColor.800" flex="0.4">
+                {benificiary?.program_beneficiaries?.enrolled_for_board
+                  ? benificiary?.program_beneficiaries?.enrolled_for_board
+                  : "-"}
+              </FrontEndTypo.H3>
+            </HStack>
+
+            <HStack
+              alignItems="Center"
+              justifyContent="space-between"
+              borderBottomWidth="1px"
+              borderBottomColor="appliedColor"
+            >
+              <FrontEndTypo.H3 color="textGreyColor.50" flex="0.3" pb="2">
+                {t("ENROLLMENT_NUMBER")}
+              </FrontEndTypo.H3>
+
+              <FrontEndTypo.H3 color="textGreyColor.800" flex="0.4">
+                {benificiary?.program_beneficiaries?.enrollment_number
+                  ? benificiary?.program_beneficiaries?.enrollment_number
+                  : "-"}
+              </FrontEndTypo.H3>
+            </HStack>
+
+            <HStack
+              alignItems="Center"
+              borderBottomWidth="1px"
+              borderBottomColor="appliedColor"
+            >
+              <FrontEndTypo.H3 color="textGreyColor.50" flex="0.7" pb="2">
+                {t("SELECTED_SUBJECTS")}
+              </FrontEndTypo.H3>
+
+              <VStack>
+                {subject?.map((e) => {
+                  {
+                    return (
+                      <FrontEndTypo.H3
+                        color="textGreyColor.800"
+                        pb="2"
+                        flex="0.3"
+                      >
+                        {e?.name}
+                      </FrontEndTypo.H3>
+                    );
+                  }
+                })}
+              </VStack>
+            </HStack>
+          </VStack>
+        </VStack>
+
+        <VStack
+          px="5"
+          pb="3"
+          pt="2"
+          borderRadius="10px"
+          borderWidth="1px"
+          bg="white"
+          borderColor="appliedColor"
+        >
+          <HStack justifyContent="space-between" alignItems="Center">
+            <FrontEndTypo.H3 bold color="textGreyColor.800">
+              {t("UPLOAD_RECEIPT")}
+            </FrontEndTypo.H3>
+            <IconByName
+              name="EditBoxLineIcon"
+              _icon={{ size: "20" }}
+              color="iconColor.100"
+              onPress={(e) => {
+                navigate(`/beneficiary/edit/${id}/enrollment-details`);
+              }}
+            />
+          </HStack>
+          <VStack space="5">
             <Box>
               <Progress
-                value={arrList(benificiary?.program_beneficiaries, [
-                  "type_of_enrollement",
-                  "enrollment_status",
-                  "enrolled_for_board",
-                  "enrollment_number",
-                  "subjects",
+                value={arrList(benificiary?.core_beneficiaries, [
+                  "career_aspiration",
+                  "career_aspiration_details",
                 ])}
                 size="xs"
                 colorScheme="info"
               />
             </Box>
-            <VStack space="2" pt="5">
-              <HStack
-                alignItems="Center"
-                justifyContent="space-between"
-                borderBottomWidth="1px"
-                borderBottomColor="appliedColor"
-              >
-                <FrontEndTypo.H3 color="textGreyColor.50" flex="0.3" pb="2">
-                  {t("TYPE_OF_ENROLLMENT")}
-                </FrontEndTypo.H3>
 
-                <FrontEndTypo.H3 color="textGreyColor.800" flex="0.4">
-                  {benificiary?.program_beneficiaries?.type_of_enrollement
-                    ? benificiary?.program_beneficiaries?.type_of_enrollement
-                    : "-"}
-                </FrontEndTypo.H3>
-              </HStack>
-
-              <HStack
-                alignItems="Center"
-                justifyContent="space-between"
-                borderBottomWidth="1px"
-                borderBottomColor="appliedColor"
-              >
-                <FrontEndTypo.H3 color="textGreyColor.50" flex="0.3" pb="2">
-                  {t("ENROLLMENT_STATUS")}
-                </FrontEndTypo.H3>
-
-                <FrontEndTypo.H3 color="textGreyColor.800" flex="0.4">
-                  {benificiary?.program_beneficiaries?.enrollment_status
-                    ? t(
-                        benificiary?.program_beneficiaries?.enrollment_status.toUpperCase()
-                      )
-                    : "-"}
-                </FrontEndTypo.H3>
-              </HStack>
-
-              <HStack
-                alignItems="Center"
-                justifyContent="space-between"
-                borderBottomWidth="1px"
-                borderBottomColor="appliedColor"
-              >
-                <FrontEndTypo.H3 color="textGreyColor.50" flex="0.3" pb="2">
-                  {t("ENROLLMENT_BOARD")}
-                </FrontEndTypo.H3>
-
-                <FrontEndTypo.H3 color="textGreyColor.800" flex="0.4">
-                  {benificiary?.program_beneficiaries?.enrolled_for_board
-                    ? benificiary?.program_beneficiaries?.enrolled_for_board
-                    : "-"}
-                </FrontEndTypo.H3>
-              </HStack>
-
-              <HStack
-                alignItems="Center"
-                justifyContent="space-between"
-                borderBottomWidth="1px"
-                borderBottomColor="appliedColor"
-              >
-                <FrontEndTypo.H3 color="textGreyColor.50" flex="0.3" pb="2">
-                  {t("ENROLLMENT_NUMBER")}
-                </FrontEndTypo.H3>
-
-                <FrontEndTypo.H3 color="textGreyColor.800" flex="0.4">
-                  {benificiary?.program_beneficiaries?.enrollment_number
-                    ? benificiary?.program_beneficiaries?.enrollment_number
-                    : "-"}
-                </FrontEndTypo.H3>
-              </HStack>
-
-              <HStack alignItems="Center" justifyContent="space-between">
-                <FrontEndTypo.H3 color="textGreyColor.50" flex="0.3">
-                  {t("SELECTED_SUBJECTS")}
-                </FrontEndTypo.H3>
-                <FrontEndTypo.H3 color="textGreyColor.800" flex="0.4">
-                  <VStack>
-                    {subject?.map((e) => {
-                      {
-                        return <React.Fragment>{e?.name}</React.Fragment>;
-                      }
-                    })}
-                  </VStack>
-                </FrontEndTypo.H3>
-              </HStack>
-            </VStack>
-          </VStack>
-
-          <VStack
-            px="5"
-            pb="3"
-            pt="2"
-            borderRadius="10px"
-            borderWidth="1px"
-            bg="white"
-            borderColor="appliedColor"
-          >
-            <HStack justifyContent="space-between" alignItems="Center">
-              <FrontEndTypo.H3 bold color="textGreyColor.800">
-                {t("UPLOAD_RECEIPT")}
-              </FrontEndTypo.H3>
-              <IconByName
-                name="EditBoxLineIcon"
-                _icon={{ size: "20" }}
-                color="iconColor.100"
-                onPress={(e) => {
-                  navigate(`/beneficiary/edit/${id}/enrollment-details`);
-                }}
-              />
-            </HStack>
-            <VStack space="5">
-              <Box>
-                <Progress
-                  value={arrList(benificiary?.core_beneficiaries, [
-                    "career_aspiration",
-                    "career_aspiration_details",
-                  ])}
-                  size="xs"
-                  colorScheme="info"
-                />
-              </Box>
-
-              <ImageView
-                source={source}
-                width="full"
-                height="172px"
-                borderRadius="5px"
-                borderWidth="1px"
-                borderColor="worksheetBoxText.100"
-                alignSelf="Center"
-              />
-            </VStack>
+            <ImageView
+              source={source}
+              width="full"
+              height="172px"
+              borderRadius="5px"
+              borderWidth="1px"
+              borderColor="worksheetBoxText.100"
+              alignSelf="Center"
+            />
           </VStack>
         </VStack>
       </VStack>
