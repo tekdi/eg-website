72e3a848207dd554d7d4fa2f4bf9535830b47272
Nikhil <nybhat@gmail.com>
2023-06-26 16:12:21 +0530

    change constant names

--- lib/common-lib/src/index.tsx
+++ lib/common-lib/src/index.tsx
@@ -80,4 +80,5 @@ export * from './services/Telemetry'
 export * from './components/calender'
 export * from './components/layout/HeaderTags/index'
 export * from './components/inputs'
+export * from './config/constant'
 
