e3e2392d9bf72b73d5051fe98b2998a08d571215
Nikhil <nybhat@gmail.com>
2023-07-05 18:32:32 +0530

    Benficiery list on IP

---
+++ apps/front-end/src/pages/admin/learner/AdminHome.js
@@ -0,0 +1,216 @@
+import React from "react";
+import Form from "@rjsf/core";
+import validator from "@rjsf/validator-ajv8";
+
+import {
+  Box,
+  HStack,
+  Select,
+  CheckIcon,
+  Text,
+  Checkbox,
+  VStack,
+  ScrollView,
+  Flex,
+  Button,
+} from "native-base";
+import {
+  IconByName,
+  AdminLayout as Layout,
+  H2,
+  useWindowSize,
+  H3,
+  t,
+  facilitatorRegistryService,
+  AdminTypo,
+  geolocationRegistryService,
+} from "@shiksha/common-lib";
+import Table from "./Table";
+import Chip from "component/Chip";
+import CustomRadio from "component/CustomRadio";
+
+export default function AdminHome({ footerLinks, userTokenInfo }) {
+  const [width, Height] = useWindowSize();
+  const [refAppBar, setRefAppBar] = React.useState();
+  const ref = React.useRef(null);
+  const [formData, setFormData] = React.useState({});
+  const [getQualificationAll, setgetQualificationAll] = React.useState();
+  const [getDistrictsAll, setgetDistrictsAll] = React.useState();
+
+  const [service, setService] = React.useState();
+  const [adminlimit, setadminLimit] = React.useState();
+  const [adminpage, setadminPage] = React.useState(1);
+  const [admindata, setadminData] = React.useState();
+  const [totalCount, settotalCount] = React.useState();
+  const [adminStatus, setadminStatus] = React.useState();
+  const [adminSearchValue, setadminSearchValue] = React.useState();
+  const [blockData, setBlockData] = React.useState();
+  let finalData;
+
+  React.useEffect(async () => {
+    let name = "RAJASTHAN";
+    const getDistricts = await geolocationRegistryService.getDistricts({
+      name,
+    });
+    setgetDistrictsAll(getDistricts?.districts);
+  }, []);
+
+  const schema = {
+    type: "object",
+    properties: {
+      DISTRICT: {
+        type: "array",
+        label: "DISTRICT",
+        items: {
+          type: "string",
+
+          enumNames: getDistrictsAll?.map((item, i) => {
+            return item?.district_name;
+          }),
+          enum: getDistrictsAll?.map((item, i) => {
+            return item?.district_name;
+          }),
+        },
+        uniqueItems: true,
+      },
+    },
+  };
+
+  const uiSchema = {
+    DISTRICT: {
+      "ui:widget": "checkboxes",
+      "ui:options": {},
+    },
+    QUALIFICATION: {
+      "ui:widget": "checkboxes",
+      "ui:options": {},
+    },
+    WORK_EXPERIENCE: {
+      "ui:widget": CustomRadio,
+    },
+  };
+
+  const onChange = async (formData) => {
+    const _formData = formData?.formData;
+    let searchValue = adminSearchValue;
+    const result = await facilitatorRegistryService.filter(
+      _formData,
+      adminpage,
+      adminlimit,
+      adminStatus,
+      searchValue
+    );
+    setadminData(result?.data?.data);
+    settotalCount(result?.data?.totalCount);
+    setFormData(_formData);
+  };
+
+  const clearFilter = () => {
+    setadminData("");
+    setFormData("");
+  };
+
+  function CustomFieldTemplate(props) {
+    const {
+      id,
+      classNames,
+      style,
+      label,
+      help,
+      required,
+      description,
+      errors,
+      children,
+    } = props;
+    return (
+      <VStack
+        className={classNames}
+        style={{ borderTopColor: "#EEEEEE", borderTopWidth: "1px" }}
+      >
+        <HStack style={{ justifyContent: "space-between" }}>
+          {id !== "root" && (
+            <HStack style={{ justifyContent: "space-between" }} width="100%">
+              <label
+                style={{
+                  fontWeight: "bold",
+                  color: "textGreyColor.400",
+                  paddingBottom: "12px",
+                }}
+              >
+                {label}
+                {required ? "*" : null}
+              </label>
+              <IconByName name="SearchLineIcon" _icon={{ size: "15px" }} />
+            </HStack>
+          )}
+        </HStack>
+        {children}
+      </VStack>
+    );
+  }
+
+  return (
+    <Layout getRefAppBar={(e) => setRefAppBar(e)} _sidebar={footerLinks}>
+      <HStack>
+        <Box
+          width="18%"
+          style={{ borderRightColor: "#EEEEEE", borderRightWidth: "2px" }}
+        >
+          <HStack ref={ref}></HStack>
+          <ScrollView
+            maxH={
+              Height - (refAppBar?.clientHeight + ref?.current?.clientHeight)
+            }
+          >
+            <VStack space={8} py="5">
+              <VStack space={3}>
+                <HStack alignItems="center" justifyContent="space-between">
+                  <HStack>
+                    <IconByName isDisabled name="FilterLineIcon" />
+                    <AdminTypo.H5 bold>{t("FILTERS")}</AdminTypo.H5>
+                  </HStack>
+                  <Button variant="link" pt="3" onPress={clearFilter}>
+                    <AdminTypo.H6 color="blueText.400" underline bold>
+                      {t("CLEAR_FILTER")}
+                    </AdminTypo.H6>
+                  </Button>
+                </HStack>
+                <Form
+                  schema={schema}
+                  uiSchema={uiSchema}
+                  onChange={onChange}
+                  validator={validator}
+                  formData={formData}
+                  templates={{
+                    FieldTemplate: CustomFieldTemplate,
+                  }}
+                >
+                  <Button display={"none"} type="submit"></Button>
+                </Form>
+              </VStack>
+            </VStack>
+          </ScrollView>
+        </Box>
+        <ScrollView
+          maxH={Height - refAppBar?.clientHeight}
+          minH={Height - refAppBar?.clientHeight}
+        >
+          <Box roundedBottom={"2xl"} py={6} px={4} mb={5}>
+            <Table
+              formData={formData}
+              admindata={admindata}
+              totalCount={totalCount}
+              setadminLimit={setadminLimit}
+              setadminPage={setadminPage}
+              setadminStatus={setadminStatus}
+              adminStatus={adminStatus}
+              setadminSearchValue={setadminSearchValue}
+              adminSearchValue={adminSearchValue}
+              facilitator={userTokenInfo?.authUser}
+            />
+          </Box>
+        </ScrollView>
+      </HStack>
+    </Layout>
+  );
+}
---
+++ apps/front-end/src/pages/admin/learner/Table.js
@@ -0,0 +1,345 @@
+import {
+  IconByName,
+  facilitatorRegistryService,
+  t,
+  ImageView,
+  AdminTypo,
+  enumRegistryService,
+} from "@shiksha/common-lib";
+import { ChipStatus } from "component/Chip";
+import Clipboard from "component/Clipboard";
+import {
+  HStack,
+  VStack,
+  Modal,
+  Image,
+  Text,
+  ScrollView,
+  Input,
+} from "native-base";
+
+import React from "react";
+import DataTable from "react-data-table-component";
+import { useNavigate } from "react-router-dom";
+const customStyles = {
+  rows: {
+    style: {
+      minHeight: "72px", // override the row height
+    },
+    style: {
+      minHeight: "72px", // override the row height
+    },
+  },
+  headCells: {
+    style: {
+      background: "#E0E0E0",
+      color: "#616161",
+      size: "16px",
+    },
+    style: {
+      background: "#E0E0E0",
+      color: "#616161",
+      size: "16px",
+    },
+  },
+  cells: {
+    style: {
+      color: "#616161",
+      size: "19px",
+    },
+    style: {
+      color: "#616161",
+      size: "19px",
+    },
+  },
+};
+const columns = (e) => [
+  {
+    name: t("NAME"),
+    selector: (row) => (
+      <HStack alignItems={"center"} space="2">
+        {row?.profile_photo_1?.name ? (
+          <ImageView
+            source={{
+              uri: row?.profile_photo_1?.name,
+            }}
+            // alt="Alternate Text"
+            width={"35px"}
+            height={"35px"}
+          />
+        ) : (
+          <IconByName
+            isDisabled
+            name="AccountCircleLineIcon"
+            color="gray.300"
+            _icon={{ size: "35" }}
+          />
+        )}
+        <AdminTypo.H5 bold>
+          {row?.first_name + " " + row.last_name}
+        </AdminTypo.H5>
+      </HStack>
+    ),
+    sortable: true,
+    attr: "name",
+  },
+  {
+    name: t("MOBILE_NUMBER"),
+    selector: (row) => row?.mobile,
+    sortable: true,
+    attr: "email",
+  },
+  {
+    name: t("DISTRICT"),
+
+    selector: (row) => (row?.district ? row?.district : "-"),
+  },
+  {
+    name: t("BLOCK"),
+
+    selector: (row) => (row?.block ? row?.block : "-"),
+  },
+
+  {
+    name: t("STATUS"),
+    selector: (row, index) => (
+      <ChipStatus key={index} status={row?.program_faciltators?.status} />
+    ),
+    sortable: true,
+    attr: "email",
+  },
+];
+
+const filters = (data, filter) => {
+  return data.filter((item) => {
+    for (let key in filter) {
+      if (
+        item[key] === undefined ||
+        !filter[key].includes(
+          `${
+            item[key] && typeof item[key] === "string"
+              ? item[key].trim()
+              : item[key]
+          }`
+        )
+      ) {
+        return false;
+      }
+    }
+
+    return true;
+  });
+};
+
+// Table component
+function Table({
+  facilitator,
+  setadminPage,
+  setadminLimit,
+  setadminStatus,
+  adminStatus,
+  adminSearchValue,
+  setadminSearchValue,
+  admindata,
+  formData,
+  totalCount,
+}) {
+  const [data, setData] = React.useState([]);
+  const [limit, setLimit] = React.useState(10);
+  const [page, setPage] = React.useState();
+  const [paginationTotalRows, setPaginationTotalRows] =
+    React.useState(totalCount);
+  // const [filterObj, setFilterObj] = React.useState();
+  const [loading, setLoading] = React.useState(true);
+  const [beneficiaryStatus, setBeneficiaryStatus] = React.useState();
+  const [status, setstatus] = React.useState("ALL");
+
+  const navigate = useNavigate();
+
+  React.useEffect(async () => {
+    setLoading(true);
+    setData(admindata);
+    setLoading(false);
+    setPaginationTotalRows(totalCount);
+  }, [admindata]);
+
+  React.useEffect(async () => {
+    setPaginationTotalRows(totalCount);
+  }, [totalCount]);
+
+  React.useEffect(async () => {
+    const result = await enumRegistryService.listOfEnum();
+    setBeneficiaryStatus(result?.data?.BENEFICIARY_STATUS);
+  }, []);
+
+  React.useEffect(async () => {
+    setLoading(true);
+
+    let _formData = formData;
+    let adminpage = page;
+    let adminlimit = limit;
+    let searchValue = adminSearchValue;
+
+    const result = await facilitatorRegistryService.filter(
+      _formData,
+      adminpage,
+      adminlimit,
+      status,
+      searchValue
+    );
+    setData(result.data?.data);
+    setPaginationTotalRows(result?.data?.totalCount);
+    setLoading(false);
+  }, [page, limit, formData]);
+
+  const exportPrerakCSV = async () => {
+    const result = await facilitatorRegistryService.exportFacilitatorsCsv();
+  };
+
+  const filterByStatus = async (value) => {
+    setLoading(true);
+    setstatus(value);
+    setadminStatus(value);
+    let _formData = formData;
+    let adminpage = page;
+    let adminlimit = limit;
+    let adminStatus = value;
+    let searchValue = adminSearchValue;
+
+    const result = await facilitatorRegistryService.filter(
+      _formData,
+      adminpage,
+      adminlimit,
+      adminStatus,
+      searchValue
+    );
+    setData(result.data?.data);
+    setPaginationTotalRows(result?.data?.totalCount);
+    // setLimit(result?.limit);
+    setLoading(false);
+  };
+
+  const searchName = async (e) => {
+    let searchValue = e?.nativeEvent?.text;
+    setadminSearchValue(searchValue);
+    let _formData = formData;
+    let adminpage = page;
+    let adminlimit = limit;
+    let status = adminStatus;
+    const result = await facilitatorRegistryService.filter(
+      _formData,
+      adminpage,
+      adminlimit,
+      status,
+      searchValue
+    );
+    setData(result.data?.data);
+    setPaginationTotalRows(result?.data?.totalCount);
+    // setLimit(result?.limit);
+    setLoading(false);
+  };
+
+  return (
+    <VStack>
+      <HStack my="1" mb="3" justifyContent="space-between">
+        <HStack justifyContent="space-between" alignItems="center">
+          <Image
+            source={{
+              uri: "/profile.svg",
+            }}
+            alt=""
+            size={"xs"}
+            resizeMode="contain"
+          />
+          <AdminTypo.H1 px="5">{t("All_AG_LEARNERS")}</AdminTypo.H1>
+          <Image
+            source={{
+              uri: "/box.svg",
+            }}
+            alt=""
+            size={"28px"}
+            resizeMode="contain"
+          />
+        </HStack>
+        <Input
+          InputLeftElement={
+            <IconByName color="coolGray.500" name="SearchLineIcon" />
+          }
+          placeholder="search"
+          variant="outline"
+          onChange={(e) => {
+            searchName(e);
+          }}
+        />
+        <HStack space={2}>
+          <AdminTypo.Secondarybutton
+            onPress={() => {
+              exportPrerakCSV();
+            }}
+            rightIcon={
+              <IconByName
+                color="#084B82"
+                _icon={{}}
+                size="15px"
+                name="ShareLineIcon"
+              />
+            }
+          >
+            {t("EXPORT")}
+          </AdminTypo.Secondarybutton>
+        </HStack>
+      </HStack>
+      <ScrollView horizontal={true} mb="2">
+        <HStack pb="2">
+          <Text
+            cursor={"pointer"}
+            mx={3}
+            onPress={() => {
+              filterByStatus("ALL");
+            }}
+          >
+            {t("BENEFICIARY_ALL")}
+            {status == "ALL" && `(${paginationTotalRows})`}
+          </Text>
+          {beneficiaryStatus?.map((item) => {
+            return (
+              <Text
+                color={status == t(item?.value) ? "blueText.400" : ""}
+                bold={status == t(item?.value) ? true : false}
+                cursor={"pointer"}
+                mx={3}
+                onPress={() => {
+                  filterByStatus(item?.value);
+                }}
+              >
+                {t(item?.title)}
+                {status == t(item?.value) && `(${paginationTotalRows})`}
+              </Text>
+            );
+          })}
+        </HStack>
+      </ScrollView>
+      <DataTable
+        customStyles={customStyles}
+        columns={[...columns()]}
+        data={data}
+        persistTableHead
+        progressPending={loading}
+        pagination
+        paginationRowsPerPageOptions={[10, 15, 25, 50, 100]}
+        paginationServer
+        paginationTotalRows={paginationTotalRows}
+        onChangeRowsPerPage={(e) => {
+          setLimit(e);
+          setadminLimit(e);
+        }}
+        onChangePage={(e) => {
+          setPage(e);
+          setadminPage(e);
+        }}
+      />
+    </VStack>
+  );
+}
+
+export default Table;
