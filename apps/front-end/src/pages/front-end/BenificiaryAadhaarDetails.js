88c9c6c4a28bb0e369702ca5cc87adc04a4a4e28
swapnilphalke03 <133649148+swapnilphalke03@users.noreply.github.com>
2023-07-07 10:47:03 +0530

    doc checklist showing null, lat long readonly, adhaar btn (#283)
    * Edit Adhaar and Age validation
    
    * doc checklist showing null, lat long readonly, adhaar btn

--- apps/front-end/src/pages/front-end/BenificiaryAadhaarDetails.js
+++ apps/front-end/src/pages/front-end/BenificiaryAadhaarDetails.js
@@ -167,6 +167,7 @@ export default function BenificiaryAadhaarDetails() {
                     {t("SCAN_QR_CODE")}
                   </FrontEndTypo.Secondarybutton> */}
                   <FrontEndTypo.Primarybutton
+                    mt="10"
                     onPress={() => {
                       navigate(`/aadhaar-kyc/${id}/upload`, {
                         state: `/beneficiary/${id}/aadhaardetails`,
