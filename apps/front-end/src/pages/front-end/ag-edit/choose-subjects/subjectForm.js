bcee622ba504492a006476a82a82b5aea0269357
Nikhil <nybhat@gmail.com>
2023-07-14 14:27:27 +0530

    added loader in upload file and on save button

--- apps/front-end/src/pages/front-end/ag-edit/choose-subjects/subjectForm.js
+++ apps/front-end/src/pages/front-end/ag-edit/choose-subjects/subjectForm.js
@@ -53,6 +53,8 @@ export default function App({ facilitator, id, ip, onClick }) {
   const [source, setSource] = React.useState();
   const [benificiary, setBenificiary] = React.useState();
   const [notMatched, setNotMatched] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
   const buttonStyle = {
     borderWidth: "2px",
     borderStyle: "dotted",
@@ -572,6 +574,7 @@ export default function App({ facilitator, id, ip, onClick }) {
       for (let key in item) {
         form_data.append(key, item[key]);
       }
+      setLoading(true);
       const uploadDoc = await uploadRegistryService.uploadFile(form_data);
       const id = uploadDoc?.data?.insert_documents?.returning[0]?.id;
       setFormData({ ...formData, ["payment_receipt_document_id"]: id });
@@ -579,6 +582,7 @@ export default function App({ facilitator, id, ip, onClick }) {
       setSource({
         document_id: uploadDoc?.data?.insert_documents?.returning?.[0].id,
       });
+      setLoading(false);
     } else {
       const newErrors = {};
       newErrors.payment_receipt_document_id = {
@@ -730,6 +734,7 @@ export default function App({ facilitator, id, ip, onClick }) {
                     </VStack>
                     <Button
                       variant={"secondary"}
+                      isLoading={loading}
                       leftIcon={
                         <IconByName name="Upload2FillIcon" isDisabled />
                       }
@@ -769,6 +774,7 @@ export default function App({ facilitator, id, ip, onClick }) {
             <FrontEndTypo.Primarybutton
               mt="3"
               type="submit"
+              isLoading={loading}
               onPress={() => {
                 editSubmit();
               }}
