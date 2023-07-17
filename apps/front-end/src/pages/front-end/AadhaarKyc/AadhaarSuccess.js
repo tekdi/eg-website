dd122ed19731fc4aad6fea3d348c75c6b4b9c4f4
Nikhil <nybhat@gmail.com>
2023-07-17 16:16:44 +0530

    bugs_resolved

--- apps/front-end/src/pages/front-end/AadhaarKyc/AadhaarSuccess.js
+++ apps/front-end/src/pages/front-end/AadhaarKyc/AadhaarSuccess.js
@@ -125,7 +125,7 @@ export default function AadhaarSuccess({
           if (location?.state) {
             navigate(location?.state);
           } else {
-            navigate("/beneficiary/list");
+            navigate("/profile");
           }
         }}
       >
