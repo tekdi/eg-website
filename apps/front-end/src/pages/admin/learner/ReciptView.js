ae01a79f25871b3eb99064d0f30d1769dd9a62b7
swapnilphalke03 <swapnil.phalke@tekditechnologies.com>
2023-08-09 17:49:12 +0530

    checkbox changes

--- apps/front-end/src/pages/admin/learner/ReciptView.js
+++ apps/front-end/src/pages/admin/learner/ReciptView.js
@@ -23,6 +23,7 @@ import {
   Input,
   Image,
   useToast,
+  Checkbox,
 } from "native-base";
 import { ChipStatus } from "component/Chip";
 import NotFound from "../../NotFound";
@@ -61,8 +62,8 @@ export default function ReciptView({ footerLinks }) {
   const { id } = useParams();
   const [data, setData] = React.useState();
   const [scale, setScale] = React.useState({
-    width: 40,
-    height: 45,
+    width: 60,
+    height: 70,
   });
 
   const navigate = useNavigate();
@@ -80,14 +81,14 @@ export default function ReciptView({ footerLinks }) {
     if (deltaY < 0) {
       setScale({
         ...scale,
-        width: scale?.width > 40 ? scale?.width - 1 : 40,
-        height: scale?.height > 45 ? scale?.height - 1 : 45,
+        width: scale?.width > 60 ? scale?.width - 1 : 60,
+        height: scale?.height > 70 ? scale?.height - 1 : 70,
       });
     } else {
       setScale({
         ...scale,
-        width: scale?.width < 90 ? scale?.width + 1 : 40,
-        height: scale?.height < 85 ? scale?.height + 1 : 45,
+        width: scale?.width < 110 ? scale?.width + 1 : 60,
+        height: scale?.height < 105 ? scale?.height + 1 : 70,
       });
     }
   };
@@ -126,62 +127,118 @@ export default function ReciptView({ footerLinks }) {
             </AdminTypo.H1>
           </HStack>
 
-          <VStack space={"5"} p="5" mt="6">
+          <VStack space={"5"}>
             <AdminTypo.H4 color="textGreyColor.800" bold>
-              {t("PAYMENT_RECEIPT").toUpperCase()}
+              {t("ENROLLMENT_DETAILS_VERIFICATION")}
             </AdminTypo.H4>
 
             <VStack space={"5"} w="100%" bg="light.100" p="6" rounded="xl">
-              <HStack justifyContent={"space-between"}>
-                <HStack
+              <HStack justifyContent={"space-evenly"}>
+                <VStack
                   justifyContent="space-between"
                   alignItems="top"
                   borderColor="light.400"
                   pb="1"
                 >
-                  <VStack>
-                    <AdminTypo.H4 color={"light.400"}>
-                      {t("TYPE_OF_ENROLLMENT")}
-                    </AdminTypo.H4>
-                    <AdminTypo.H3>
-                      {data?.program_beneficiaries?.enrollment_status}
-                    </AdminTypo.H3>
-                  </VStack>
-                  <VStack mx={3}>
-                    <AdminTypo.H4 color={"light.400"}>
-                      {t("ENROLLMENT_STATUS")}
-                    </AdminTypo.H4>
-                    <AdminTypo.H3>
-                      {data?.program_beneficiaries?.enrollment_status}
-                    </AdminTypo.H3>
-                  </VStack>
-                  <VStack>
-                    <AdminTypo.H4 color={"light.400"}>
-                      {t("ENROLLMENT_BOARD")}
-                    </AdminTypo.H4>
-                    <AdminTypo.H3>
-                      {data?.program_beneficiaries?.enrolled_for_board}
-                    </AdminTypo.H3>
-                  </VStack>
-                </HStack>
-                <ImageView
-                  source={{
-                    document_id:
-                      data?.program_beneficiaries?.payment_receipt_document_id,
-                  }}
-                  alt="aadhaar_front"
-                  width={scale?.width + "vw"}
-                  height={scale?.height + "vh"}
-                  maxWidth={"90vh"}
-                  minWidth={"40vw"}
-                  maxHeight={"85vh"}
-                  minHeight={"45vh"}
-                  borderRadius="5px"
-                  borderWidth="1px"
-                  borderColor="worksheetBoxText.100"
-                  alignSelf="Center"
-                  onWheel={handleScroll}
-                />
+                  <HStack>
+                    <VStack mx={5}>
+                      <AdminTypo.H5 color={"light.400"}>
+                        {t("ENROLLMENT_STATUS")}
+                      </AdminTypo.H5>
+                      <AdminTypo.H3>
+                        {data?.program_beneficiaries?.enrollment_status}
+                      </AdminTypo.H3>
+                    </VStack>
+                    <VStack mx={5}>
+                      <AdminTypo.H5 color={"light.400"}>
+                        {t("ENROLLMENT_BOARD")}
+                      </AdminTypo.H5>
+                      <AdminTypo.H3>
+                        {data?.program_beneficiaries?.enrolled_for_board}
+                      </AdminTypo.H3>
+                    </VStack>
+                  </HStack>
+                  <HStack my={3}>
+                    <Checkbox />
+                    <VStack ml={3}>
+                      <AdminTypo.H4 color={"light.400"}>
+                        {t("ENROLLMENT_NUMBER")}
+                      </AdminTypo.H4>
+                      <AdminTypo.H3>
+                        {data?.program_beneficiaries?.enrollment_number}
+                      </AdminTypo.H3>
+                    </VStack>
+                  </HStack>
+                  <HStack my={3}>
+                    <Checkbox />
+                    <VStack ml={3}>
+                      <AdminTypo.H4 color={"light.400"}>
+                        {t("SELECTED_SUBJECTS")}
+                      </AdminTypo.H4>
+                      <AdminTypo.H3>
+                        {data?.program_beneficiaries?.enrolled_for_board}
+                      </AdminTypo.H3>
+                    </VStack>
+                  </HStack>
+                  <HStack my={3}>
+                    <Checkbox />
+                    <VStack ml={3}>
+                      <AdminTypo.H4 color={"light.400"}>
+                        {t("NAME")}
+                      </AdminTypo.H4>
+                      <AdminTypo.H3>
+                        {data?.program_beneficiaries?.enrollment_first_name}
+                        {data?.program_beneficiaries?.enrollment_last_name &&
+                          " " +
+                            data?.program_beneficiaries?.enrollment_last_name}
+                      </AdminTypo.H3>
+                    </VStack>
+                  </HStack>
+                  <HStack my={3}>
+                    <Checkbox />
+                    <VStack ml={3}>
+                      <AdminTypo.H4 color={"light.400"}>
+                        {t("DOB")}
+                      </AdminTypo.H4>
+                      <AdminTypo.H3>
+                        {data?.program_beneficiaries?.enrollment_dob}
+                      </AdminTypo.H3>
+                    </VStack>
+                  </HStack>
+                  <HStack my={3}>
+                    <Checkbox />
+                    <VStack ml={3}>
+                      <AdminTypo.H4 color={"light.400"}>
+                        {t("AADHAAR_NUMBER")}
+                      </AdminTypo.H4>
+                      <AdminTypo.H3>
+                        {data?.program_beneficiaries?.enrollment_aadhaar_no}
+                      </AdminTypo.H3>
+                    </VStack>
+                  </HStack>
+                </VStack>
+
+                <VStack width={"60vw"} height={"70vh"}>
+                  <ImageView
+                    source={{
+                      document_id:
+                        data?.program_beneficiaries
+                          ?.payment_receipt_document_id,
+                    }}
+                    alt="aadhaar_front"
+                    width={scale?.width + "vw"}
+                    height={scale?.height + "vh"}
+                    maxWidth={"110vW"}
+                    minWidth={"60vw"}
+                    maxHeight={"105vh"}
+                    minHeight={"70vh"}
+                    borderRadius="5px"
+                    borderWidth="1px"
+                    borderColor="worksheetBoxText.100"
+                    alignSelf="Center"
+                    onWheel={handleScroll}
+                  />
+                </VStack>
               </HStack>
               <HStack width={"100%"}>
                 <AdminTypo.PrimaryButton>
