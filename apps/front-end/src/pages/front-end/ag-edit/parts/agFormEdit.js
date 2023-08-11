9b197aa1b34cec77645ab42e9de1008051d2877d
swapnilphalke03 <swapnil.phalke@tekditechnologies.com>
2023-08-11 15:51:25 +0530

    AgFormEdit basicDetails form not working

--- apps/front-end/src/pages/front-end/ag-edit/parts/agFormEdit.js
+++ apps/front-end/src/pages/front-end/ag-edit/parts/agFormEdit.js
@@ -2,24 +2,9 @@ import React, { useState } from "react";
 import Form from "@rjsf/core";
 import validator from "@rjsf/validator-ajv8";
 import schema1 from "./schema.js";
+import { Alert, Box, Button, HStack, Modal, VStack } from "native-base";
+
 import {
-  Alert,
-  Box,
-  Button,
-  Center,
-  HStack,
-  Image,
-  Modal,
-  Radio,
-  Stack,
-  VStack,
-} from "native-base";
-import CustomRadio from "../../../../component/CustomRadio.js";
-import Steper from "../../../../component/Steper.js";
-import {
-  facilitatorRegistryService,
-  geolocationRegistryService,
-  Camera,
   Layout,
   H1,
   t,
@@ -27,15 +12,9 @@ import {
   H3,
   IconByName,
   BodySmall,
-  filtersByObject,
-  H2,
-  getBase64,
   BodyMedium,
-  changeLanguage,
-  enumRegistryService,
   benificiaryRegistoryService,
   AgRegistryService,
-  uploadRegistryService,
   FrontEndTypo,
 } from "@shiksha/common-lib";
 import moment from "moment";
@@ -93,69 +72,12 @@ export default function agFormEdit({ ip, id }) {
     const qData = getdata?.result;
     setFormData({
       ...formData,
-      middle_name: qData?.middle_name == "null" ? "" : qData?.middle_name,
+      middle_name: qData?.middle_name,
       first_name: qData?.first_name,
-      last_name: qData?.last_name == "null" ? "" : qData?.last_name,
+      last_name: qData?.last_name,
       dob: qData?.dob && qData?.dob,
     });
   }, []);
-
-  React.useEffect(async () => {
-    const updateDetails = await AgRegistryService.updateAg(formData, userId);
-
-    if (page === "2") {
-      setFormData({
-        ...formData,
-        edit_page_type: "edit_contact",
-        device_ownership: device_ownership,
-        device_type: device_type,
-        mark_as_whatsapp_number: mark_as_whatsapp_number,
-        alternative_device_ownership: alternative_device_ownership,
-        alternative_device_type: alternative_device_type,
-        //father_first_name: father_first_name,
-        // father_middle_name: father_middle_name,
-        // mother_first_name: mother_first_name,
-        // mother_middle_name: mother_middle_name,
-        // mother_last_name: mother_last_name,
-      });
-    } else if (page === "3") {
-      const updateDetails = await AgRegistryService.updateAg(formData, userId);
-      setFormData({ ...formData, edit_page_type: "edit_address" });
-    } else if (page === "4") {
-      const updateDetails = await AgRegistryService.updateAg(formData, userId);
-      setFormData({
-        ...formData,
-        edit_page_type: "personal",
-        marital_status: marital_status,
-        social_category: social_category,
-      });
-    } else if (page === "5") {
-      const updateDetails = await AgRegistryService.updateAg(formData, userId);
-      setFormData({
-        ...formData,
-        edit_page_type: "edit_family",
-      });
-    } else if (page === "6") {
-      setFormData({
-        ...formData,
-        fatherdetails: {
-          father_first_name: father_first_name,
-          father_last_name: father_last_name,
-          father_middle_name: father_middle_name,
-        },
-        motherdetails: {
-          mother_first_name: mother_first_name,
-          mother_middle_name: mother_middle_name,
-          mother_last_name: mother_last_name,
-        },
-      });
-    } else if (page === "7") {
-      const updateDetails = await AgRegistryService.updateAg(
-        formData?.fatherdetails,
-        userId
-      );
-    }
-  }, [page]);
 
   const uiSchema = {
     dob: {
@@ -164,44 +86,6 @@ export default function agFormEdit({ ip, id }) {
         yearsRange: yearsRange,
         hideNowButton: true,
         hideClearButton: true,
-      },
-    },
-    qualification: {
-      "ui:widget": CustomR,
-    },
-    degree: {
-      "ui:widget": CustomR,
-    },
-    gender: {
-      "ui:widget": CustomR,
-    },
-    sourcing_channel: {
-      "ui:widget": CustomR,
-    },
-    availability: {
-      "ui:widget": RadioBtn,
-    },
-
-    experience: {
-      related_to_teaching: {
-        "ui:widget": RadioBtn,
-      },
-    },
-
-    vo_experience: {
-      items: {
-        experience_in_years: { "ui:widget": CustomR },
-        related_to_teaching: {
-          "ui:widget": RadioBtn,
-        },
-      },
-    },
-    experience: {
-      items: {
-        experience_in_years: { "ui:widget": CustomR },
-        related_to_teaching: {
-          "ui:widget": RadioBtn,
-        },
       },
     },
   };
@@ -242,58 +126,6 @@ export default function agFormEdit({ ip, id }) {
     }
   };
 
-  const getOptions = (schema, { key, arr, title, value, filters } = {}) => {
-    let enumObj = {};
-    let arrData = arr;
-    if (!_.isEmpty(filters)) {
-      arrData = filtersByObject(arr, filters);
-    }
-    enumObj = {
-      ...enumObj,
-      ["enumNames"]: arrData.map((e) => `${e?.[title]}`),
-    };
-    enumObj = { ...enumObj, ["enum"]: arrData.map((e) => `${e?.[value]}`) };
-    const newProperties = schema["properties"][key];
-    let properties = {};
-    if (newProperties) {
-      if (newProperties.enum) delete newProperties.enum;
-      let { enumNames, ...remainData } = newProperties;
-      properties = remainData;
-    }
-    return {
-      ...schema,
-      ["properties"]: {
-        ...schema["properties"],
-        [key]: {
-          ...properties,
-          ...(_.isEmpty(arr) ? {} : enumObj),
-        },
-      },
-    };
-  };
-
-  React.useEffect(async () => {
-    if (schema?.properties?.state) {
-      const qData = await geolocationRegistryService.getStates();
-      let newSchema = schema;
-      if (schema["properties"]["state"]) {
-        newSchema = getOptions(newSchema, {
-          key: "state",
-          arr: qData?.states,
-          title: "state_name",
-          value: "state_name",
-        });
-      }
-      newSchema = await setDistric({
-        schemaData: newSchema,
-        state: formData?.state,
-        district: formData?.district,
-        block: formData?.block,
-      });
-      setSchema(newSchema);
-    }
-  }, [page]);
-
   React.useEffect(() => {
     if (schema1.type === "step") {
       const properties = schema1.properties;
@@ -307,40 +139,6 @@ export default function agFormEdit({ ip, id }) {
       setSubmitBtn(t("NEXT"));
     }
   }, []);
-
-  const updateBtnText = () => {
-    if (schema?.properties?.vo_experience) {
-      if (formData.vo_experience?.length > 0) {
-        setSubmitBtn(t("NEXT"));
-        setAddBtn(t("ADD_EXPERIENCE"));
-      } else {
-        setSubmitBtn(t("NO"));
-        setAddBtn(t("YES"));
-      }
-    } else if (schema?.properties?.mobile) {
-      setSubmitBtn(t("SAVE"));
-      setAddBtn(t("ADD_EXPERIENCE"));
-    } else {
-      setSubmitBtn(t("SAVE"));
-    }
-  };
-
-  React.useEffect(() => {
-    updateBtnText();
-  }, [formData, page, lang]);
-
-  const userExist = async (filters) => {
-    return await facilitatorRegistryService.isExist(filters);
-  };
-
-  const formSubmitUpdate = async (formData) => {
-    if (id) {
-      const data = await enumRegistryService.editProfileById({
-        ...formData,
-        id: id,
-      });
-    }
-  };
 
   const goErrorPage = (key) => {
     if (key) {
@@ -400,19 +198,7 @@ export default function agFormEdit({ ip, id }) {
     setErrors();
     const newData = { ...formData, ...data };
     setFormData(newData);
-    if (id === "root_mobile") {
-      if (data?.mobile?.toString()?.length === 10) {
-        const result = await userExist({ mobile: data?.mobile });
-        if (result.isUserExist) {
-          const newErrors = {
-            mobile: {
-              __errors: [t("MOBILE_NUMBER_ALREADY_EXISTS")],
-            },
-          };
-          setErrors(newErrors);
-        }
-      }
-    }
+
     if (id === "root_dob") {
       if (data?.dob) {
         const age_in_years = moment().diff(data?.dob, "years", true);
@@ -424,6 +210,17 @@ export default function agFormEdit({ ip, id }) {
           };
           setErrors(newErrors);
         }
+      }
+    }
+
+    if (id === "root_middle_name") {
+      if (data?.middle_name === undefined) {
+        setFormData({ ...formData, middle_name: "" });
+      }
+    }
+    if (id === "root_last_name") {
+      if (data?.last_name === undefined) {
+        setFormData({ ...formData, last_name: "" });
       }
     }
   };
@@ -439,213 +236,6 @@ export default function agFormEdit({ ip, id }) {
   const onSubmit = async (data) => {
     const updateDetails = await AgRegistryService.updateAg(formData, userId);
     navigate(`/beneficiary/${userId}/basicdetails`);
-  };
-
-  const [cameraFile, setcameraFile] = useState();
-
-  const handleFileInputChange = async (e) => {
-    let file = e.target.files[0];
-    if (file.size <= 1048576 * 2) {
-      const data = await getBase64(file);
-      setCameraUrl(data);
-      setcameraFile(file);
-      setFormData({ ...formData, ["profile_url"]: data });
-    } else {
-      setErrors({ fileSize: t("FILE_SIZE") });
-    }
-  };
-
-  const uploadProfile = async () => {
-    // const { id } = authUser;
-    if (userId) {
-      const form_data = new FormData();
-      const item = {
-        file: cameraFile,
-        document_type: "profile",
-        user_id: userId,
-      };
-      for (let key in item) {
-        form_data.append(key, item[key]);
-      }
-
-      const uploadDoc = await uploadRegistryService.uploadFile(form_data);
-    }
-  };
-
-  if (cameraUrl) {
-    return (
-      <Layout
-        _appBar={{
-          lang,
-          setLang,
-          onPressBackButton: (e) => {
-            setCameraUrl();
-            setCameraModal(false);
-          },
-        }}
-        _page={{ _scollView: { bg: "white" } }}
-      >
-        <VStack py={6} px={4} mb={5} space="6">
-          {/* add the profile image */}
-          <H1 color="red.1000">{t("ADD_ID_PHOTOS")}</H1>
-          <h5 color="red.1000" fontSize="3">
-            {t("CLEAR_PROFILE_MESSAGE")}
-          </h5>
-
-          <Center>
-            <Image
-              source={{
-                uri: cameraUrl,
-                //show the image
-              }}
-              alt=""
-              size="324px"
-            />
-          </Center>
-          <h5 color="red.1000" fontSize="2">
-            {t("FRONT_VIEW")}
-          </h5>
-          <FrontEndTypo.Primarybutton
-            onPress={async (e) => {
-              if (cameraSelection >= 2) {
-                nextPreviewStep();
-                setCameraUrl();
-                setCameraModal(false);
-                pages[pages?.length - 1] === page;
-              } else {
-                console.log("reached else ==>");
-                uploadProfile();
-                setCameraUrl();
-                setCameraModal(true);
-                setCameraSelection(cameraSelection + 1);
-              }
-              /*  await formSubmitUpdate({ ...formData, form_step_number: "5" });
-              console.log(...formData);
-              if (onClick) onClick(cameraUrl); */
-            }}
-          >
-            {cameraSelection > 2 ? setSubmitBtn(t("SAVE")) : t("SAVE")}
-          </FrontEndTypo.Primarybutton>
-          <FrontEndTypo.Secondarybutton
-            leftIcon={<IconByName name="CameraLineIcon" isDisabled />}
-            onPress={(e) => {
-              setCameraUrl();
-              setCameraModal(true);
-            }}
-          >
-            {t("TAKE_ANOTHER_PHOTO")}
-          </FrontEndTypo.Secondarybutton>
-        </VStack>
-      </Layout>
-    );
-  }
-  if (cameraModal) {
-    return (
-      <Camera
-        {...{
-          cameraModal,
-          setCameraModal,
-          cameraUrl,
-          setcameraFile,
-          setCameraUrl: async (url) => {
-            setCameraUrl(url);
-            setCameraData([...cameraData, url]);
-            setFormData({ ...formData, ["profile_url"]: cameraData });
-          },
-        }}
-      />
-    );
-  }
-
-  if (page === "5") {
-    return (
-      <Layout
-        _appBar={{
-          onPressBackButton: (e) => setPage("4"),
-          lang,
-          setLang,
-          onlyIconsShow: ["backBtn", "userInfo"],
-        }}
-        _page={{ _scollView: { bg: "white" } }}
-      >
-        <VStack py={6} px={4} mb={5} space="6">
-          <H1 color="red.1000">{t("ADD_ID_PHOTOS")}</H1>
-          <h5 color="red.1000" fontSize="3">
-            {t("CLEAR_PROFILE_MESSAGE")}
-          </h5>
-          <Button
-            variant={"primary"}
-            leftIcon={
-              <IconByName
-                name="CameraLineIcon"
-                color="white"
-                size={2}
-                isDisabled
-              />
-            }
-            onPress={(e) => {
-              setCameraUrl();
-              setCameraModal(true);
-            }}
-          >
-            {t("TAKE_PHOTO")}
-          </Button>
-          <VStack space={2}>
-            <input
-              accept="image/*"
-              type="file"
-              style={{ display: "none" }}
-              ref={uplodInputRef}
-              onChange={handleFileInputChange}
-            />
-            {errors?.fileSize ? (
-              <H2 color="red.400">{errors?.fileSize}</H2>
-            ) : (
-              <React.Fragment />
-            )}
-          </VStack>
-        </VStack>
-      </Layout>
-    );
-  }
-
-  const AddButton = ({ icon, iconType, ...btnProps }) => {
-    return (
-      <Button
-        variant={"outlinePrimary"}
-        colorScheme="green"
-        {...btnProps}
-        onPress={(e) => {
-          updateBtnText();
-          if (formRef?.current.validateForm()) {
-            btnProps?.onClick();
-          }
-        }}
-      >
-        <HStack>
-          {icon} {addBtn}
-        </HStack>
-      </Button>
-    );
-  };
-
-  const RemoveButton = ({ icon, iconType, ...btnProps }) => {
-    return (
-      <Button
-        variant={"outlinePrimary"}
-        colorScheme="red"
-        mb="2"
-        {...btnProps}
-        onPress={(e) => {
-          updateBtnText();
-          btnProps?.onClick();
-        }}
-      >
-        <HStack>
-          {icon} {t("REMOVE_EXPERIENCE")}
-        </HStack>
-      </Button>
-    );
   };
 
   return (
@@ -676,7 +266,6 @@ export default function agFormEdit({ ip, id }) {
             ref={formRef}
             widgets={{ RadioBtn, CustomR }}
             templates={{
-              ButtonTemplates: { AddButton, RemoveButton },
               FieldTemplate,
               ArrayFieldTitleTemplate,
               ObjectFieldTemplate,
