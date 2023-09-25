3ee58284d5e6ba8a1efdd515848bff74e4744060
Chaitanya <chaitanya.kole@tekditechnologies.com>
2023-09-25 12:03:15 +0530

    unused change

--- apps/front-end/src/pages/front-end/BenificiaryAadhaarDetails.js
+++ apps/front-end/src/pages/front-end/BenificiaryAadhaarDetails.js
@@ -167,13 +167,13 @@ export default function BenificiaryAadhaarDetails() {
                       {t("AADHAAR_NUMBER_KYC")}
                     </FrontEndTypo.Primarybutton>
                     {/* <FrontEndTypo.Secondarybutton
-                      my="4"
-                      onPress={() => {
-                        navigate(`/aadhaar-kyc/${id}/QR`);
-                      }}
-                    >
-                      {t("SCAN_QR_CODE")}
-                    </FrontEndTypo.Secondarybutton> */}
+                    my="4"
+                    onPress={() => {
+                      navigate(`/aadhaar-kyc/${id}/QR`);
+                    }}
+                  >
+                    {t("SCAN_QR_CODE")}
+                  </FrontEndTypo.Secondarybutton> */}
                     <FrontEndTypo.Primarybutton
                       mt="10"
                       onPress={() => {
