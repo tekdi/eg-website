044ba665dbd8ae5147a734e7314ac2db3c412c4a
Chaitanya <chaitanya.kole@tekditechnologies.com>
2023-09-26 14:16:12 +0530

    Changes

--- apps/front-end/src/pages/front-end/Community/CommunityForm.js
+++ apps/front-end/src/pages/front-end/Community/CommunityForm.js
@@ -1,63 +1,44 @@
-import React from "react";
-import Form from "@rjsf/core";
-import validator from "@rjsf/validator-ajv8";
-import schema1 from "./schema.js";
-import { Box, Button } from "native-base";
-import { Layout } from "@shiksha/common-lib";
-import { useNavigate, useParams } from "react-router-dom";
-import {
-  RadioBtn,
-  CustomR,
-} from "../../../../../front-end/src/component/BaseInput.js";
-import { useTranslation } from "react-i18next";
-
-export default function CommunityForm({ ip }) {
-  const [schema, setSchema] = React.useState({});
-  const formRef = React.useRef();
-  const { t } = useTranslation();
-  const navigate = useNavigate();
-  const onPressBackButton = async () => {
-    navigate(`/community/list`);
-  };
-
-  const uiSchema = {};
-
-  React.useEffect(() => {
-    if (schema1.type === "step") {
-      const properties = schema1.properties.properties;
-      const newSteps = Object.keys(properties);
-      setSchema(properties[newSteps[0]]);
-    }
-  }, []);
-
-  return (
-    <Layout
-      _appBar={{
-        onPressBackButton,
-        name: t("COMMUNITY_DETAILS"),
-      }}
-      _page={{ _scollView: { bg: "white" } }}
-    >
-      <Box py={6} px={4} mb={5}>
-        <Form
-          ref={formRef}
-          widgets={{ RadioBtn, CustomR }}
-          {...{
-            validator,
-            schema: schema ? schema : {},
-            uiSchema,
-          }}
-        >
-          <Button
-            mt="3"
-            variant={"primary"}
-            type="submit"
-            onPress={() => formRef?.current?.submit()}
-          >
-            {t("SAVE")}
-          </Button>
-        </Form>
-      </Box>
-    </Layout>
-  );
-}
+export default {
+  description: "1.Basic details",
+  type: "step",
+  properties: {
+    1: {
+      title: "Community Details",
+      type: "object",
+      properties: {
+        referencefullname: {
+          title: "FULL_NAME",
+          required: ["first_name", "contact_number", "relation"],
+          type: "object",
+          properties: {
+            first_name: {
+              type: "string",
+              title: "FIRST_NAME",
+            },
+            middle_name: {
+              type: ["string", "null"],
+              title: "MIDDLE_NAME",
+            },
+            last_name: {
+              type: ["string", "null"],
+              title: "LAST_NAME",
+            },
+            relation: {
+              type: "string",
+              title: "DESIGNATION",
+            },
+            contact_number: {
+              type: "number",
+              title: "MOBILE_NUMBER",
+            },
+            edit_page_type: {
+              type: "string",
+              default: "edit_reference",
+              format: "hidden",
+            },
+          },
+        },
+      },
+    },
+  },
+};
--- apps/front-end/src/pages/front-end/Community/CommunityView.js
+++ apps/front-end/src/pages/front-end/Community/CommunityView.js
@@ -1,86 +1,157 @@
-import React from "react";
-// import Form from "./Form";
 import { FrontEndTypo, Layout } from "@shiksha/common-lib";
-import { HStack, VStack } from "native-base";
-import { useNavigate } from "react-router-dom";
-import DataTable from "react-data-table-component";
-
-export const CustomStyles = {
-  rows: {
-    style: {
-      minHeight: "72px",
-    },
-  },
-  headCells: {
-    style: {
-      background: "#E0E0E0",
-      color: "#616161",
-      size: "16px",
-      justifyContent: "center", // override the alignment of columns
-    },
-  },
-  cells: {
-    style: {
-      color: "#616161",
-      size: "19px",
-      justifyContent: "center", // override the alignment of columns
-    },
-  },
-};
-const columns = [
-  {
-    name: "Sr",
-    selector: (row) => row.id,
-  },
-  {
-    name: "Title",
-    selector: (row) => row.name,
-  },
-  {
-    name: "mobile",
-    selector: (row) => row.mobile,
-  },
-  {
-    name: "designation",
-    selector: (row) => row.designation,
-  },
-];
-
-const data = [
-  { id: 1, name: "John Doe", mobile: "123-456-7890", designation: "President" },
-  {
-    id: 2,
-    name: "Jane Smith",
-    mobile: "987-654-3210",
-    designation: "Vice President",
-  },
-];
+import React from "react";
+import {
+  widgets,
+  templates,
+  FieldTemplate,
+  TitleFieldTemplate,
+  DescriptionFieldTemplate,
+  ObjectFieldTemplate,
+  ArrayFieldTitleTemplate,
+  BaseInputTemplate,
+  RadioBtn,
+  CustomR,
+} from "component/BaseInput";
+import { useTranslation } from "react-i18next";
+import schema1 from "./CommunityForm";
+import Form from "@rjsf/core";
+import validator from "@rjsf/validator-ajv8";
+import { Box, Button } from "native-base";
 
 export default function CommunityView({ footerLinks }) {
-  const navigate = useNavigate();
+  const { t } = useTranslation();
+  const [lang, setLang] = React.useState(localStorage.getItem("lang"));
+  const [schema, setSchema] = React.useState({});
+  const [formData, setFormData] = React.useState({});
+  const [errors, setErrors] = React.useState({});
+  const [addMore, setAddMore] = React.useState();
+
+  const formRef = React.useRef();
+
+  React.useEffect(() => {
+    if (schema1.type === "step") {
+      const properties = schema1.properties;
+      const newSteps = Object.keys(properties);
+      setSchema(properties[newSteps[0]]);
+    }
+  }, []);
+
+  const transformErrors = (errors, uiSchema) => {
+    return errors.map((error) => {
+      if (error.name === "required") {
+        if (schema?.properties?.[error?.property]?.title) {
+          error.message = `${t("REQUIRED_MESSAGE")} "${t(
+            schema?.properties?.[error?.property]?.title
+          )}"`;
+        } else {
+          error.message = `${t("REQUIRED_MESSAGE")}`;
+        }
+      } else if (error.name === "enum") {
+        error.message = `${t("SELECT_MESSAGE")}`;
+      }
+      return error;
+    });
+  };
+  console.log(schema);
+  const onChange = async (e, id) => {
+    const data = e.formData;
+    setErrors();
+    const newData = { ...formData, ...data };
+    setFormData(newData);
+    if (id === "root_contact_number") {
+      if (data?.mobile?.toString()?.length === 10) {
+        const result = await userExist({ mobile: data?.mobile });
+
+        const newErrors = {
+          mobile: {
+            __errors: [t("PLEASE_ENTER_VALID_NUMBER")],
+          },
+        };
+        setErrors(newErrors);
+      }
+    }
+    setFormData(newData);
+  };
+
+  const onAdd = () => {
+    setFormData();
+    setAddMore(true);
+  };
+  const onError = (data) => {
+    if (data[0]) {
+      const key = data[0]?.property?.slice(1);
+      goErrorPage(key);
+    }
+  };
+  console.log(formData);
+  const onSubmit = async (data) => {
+    if (formData?.referencefullname?.contact_number.toString()?.length !== 10) {
+      const newErrors = {
+        contact_number: {
+          __errors: [t("PLEASE_ENTER_VALID_NUMBER")],
+        },
+      };
+      setAddMore(false);
+      setErrors(newErrors);
+    }
+  };
+
   return (
     <Layout
       _appBar={{
-        onlyIconsShow: ["userInfo", "loginBtn"],
+        onlyIconsShow: ["backBtn"],
+        leftIcon: <FrontEndTypo.H2>{t("Community Details")}</FrontEndTypo.H2>,
+        lang,
+        setLang,
         _box: { bg: "white", shadow: "appBarShadow" },
         _backBtn: { borderWidth: 1, p: 0, borderColor: "btnGray.100" },
       }}
       _page={{ _scollView: { bg: "formBg.500" } }}
       _footer={{ menues: footerLinks }}
     >
-      <VStack p="4" space="4">
-        <HStack justifyContent={"right"}>
-          <FrontEndTypo.Primarybutton>
-            <FrontEndTypo.H2
-              color={"#fff"}
-              onPress={(e) => navigate("/community/communityform")}
-            >
-              Add Community member
-            </FrontEndTypo.H2>
+      <Box p="4">
+        <Button variant={"link"} colorScheme="info">
+          <FrontEndTypo.H3 color="blueText.400" underline bold onPress={onAdd}>
+            {t("Add Communty member")}
+          </FrontEndTypo.H3>
+        </Button>
+
+        <Form
+          key={lang}
+          ref={formRef}
+          templates={{
+            FieldTemplate,
+            ArrayFieldTitleTemplate,
+            ObjectFieldTemplate,
+            TitleFieldTemplate,
+            DescriptionFieldTemplate,
+            BaseInputTemplate,
+          }}
+          extraErrors={errors}
+          showErrorList={false}
+          noHtml5Validate={true}
+          {...{
+            validator,
+            schema: schema ? schema : {},
+            formData,
+            onChange,
+            onError,
+            onSubmit,
+            transformErrors,
+          }}
+        >
+          <FrontEndTypo.Primarybutton p="4" mt="4">
+            {t("SAVE")}
           </FrontEndTypo.Primarybutton>
-        </HStack>
-        <DataTable customStyles={CustomStyles} columns={columns} data={data} />
-      </VStack>
+          <FrontEndTypo.Secondarybutton
+            p="4"
+            mt="4"
+            onPress={() => setAddMore()}
+          >
+            {t("CANCEL")}
+          </FrontEndTypo.Secondarybutton>
+        </Form>
+      </Box>
     </Layout>
   );
 }
