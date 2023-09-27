63e1529cbcec8417ecfdf9b572215aae8a75dbc6
Chaitanya <chaitanya.kole@tekditechnologies.com>
2023-09-28 00:30:07 +0530

    Commmunity view for mobile

--- apps/front-end/src/App.js
+++ apps/front-end/src/App.js
@@ -65,6 +65,11 @@ function App() {
           icon: "GraduationCap",
         },
         {
+          title: "COMMUNITY",
+          route: "/community",
+          icon: "TeamLineIcon",
+        },
+        {
           title: "MY_CAMP",
           icon: "CommunityLineIcon",
         },
---
+++ apps/front-end/src/pages/front-end/Community/CommunityForm.js
@@ -0,0 +1,32 @@
+export default {
+  title: "COMMUNITY_DETAILS",
+  type: "object",
+  required: ["first_name", "contact_number", "designation"],
+  properties: {
+    first_name: {
+      type: "string",
+      title: "FIRST_NAME",
+      regex: /^(?!.*[\u0900-\u097F])[A-Za-z\s\p{P}]+$/,
+    },
+    middle_name: {
+      type: ["string", "null"],
+      title: "MIDDLE_NAME",
+      regex: /^(?!.*[\u0900-\u097F])[A-Za-z\s\p{P}]+$/,
+    },
+    last_name: {
+      type: ["string", "null"],
+      title: "LAST_NAME",
+      regex: /^(?!.*[\u0900-\u097F])[A-Za-z\s\p{P}]+$/,
+    },
+    designation: {
+      type: "string",
+      title: "DESIGNATION",
+      format: "select",
+    },
+    contact_number: {
+      type: "string",
+      title: "MOBILE_NUMBER",
+      format: "MobileNumber",
+    },
+  },
+};
