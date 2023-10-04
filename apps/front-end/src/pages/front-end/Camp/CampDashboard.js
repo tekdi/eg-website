b33da2800d5884a46a3f418697b60d0e13956d25
swapnilphalke03 <swapnil.phalke@tekditechnologies.com>
2023-10-04 18:36:38 +0530

    206010 community task

--- apps/front-end/src/pages/front-end/Camp/CampDashboard.js
+++ apps/front-end/src/pages/front-end/Camp/CampDashboard.js
@@ -8,6 +8,7 @@ import {
   CampService,
   enumRegistryService,
   facilitatorRegistryService,
+  benificiaryRegistoryService,
 } from "@shiksha/common-lib";
 import {
   HStack,
@@ -30,6 +31,7 @@ export default function CampDashboard({ footerLinks }) {
   const [nonRegisteredUser, setNonRegisteredUser] = React.useState([]);
   const [campList, setCampList] = React.useState();
   const [enumOptions, setEnumOptions] = React.useState();
+  const [communityLength, setCommunityLength] = React.useState();
   const [ipStatus, setIpStatus] = React.useState();
 
   React.useEffect(async () => {
@@ -37,6 +39,11 @@ export default function CampDashboard({ footerLinks }) {
     const campList = await CampService.campList();
     const enums = await enumRegistryService.listOfEnum();
     const ip_user_info = await facilitatorRegistryService.getInfo();
+    const getData = await benificiaryRegistoryService.getCommunityReferences({
+      context: "community.user",
+    });
+
+    setCommunityLength(getData?.data.length || 0);
     setIpStatus(ip_user_info?.program_faciltators?.status);
     setEnumOptions(enums?.data || {});
     setNonRegisteredUser(result?.data?.user || []);
@@ -61,129 +68,146 @@ export default function CampDashboard({ footerLinks }) {
       >
         {["selected_for_onboarding", "selected_prerak"].includes(ipStatus) ? (
           <VStack>
-            <HStack
-              justifyContent={"space-between"}
-              alignItems={"center"}
-              mt={5}
-            >
-              <VStack width={"50%"}>
-                <AdminTypo.H3 color="textMaroonColor.400">
-                  {`${nonRegisteredUser?.length} `}
-                  {t("BENEFICIARY_STATUS_ENROLLED_IP_VERIFIED")}
-                </AdminTypo.H3>
-                <AdminTypo.H3 color="textMaroonColor.400">
-                  {t("LEARNERS")}
-                </AdminTypo.H3>
-              </VStack>
-              <HStack>
-                <Center>
-                  {nonRegisteredUser.length > 0 && (
-                    <Avatar.Group
-                      _avatar={{
-                        size: "lg",
-                      }}
-                      max={3}
-                    >
-                      {nonRegisteredUser?.map((item) => {
-                        return (
-                          <Avatar
-                            key={item}
-                            bg="red.500"
-                            {...(item?.profile_photo_1?.fileUrl
-                              ? {
-                                  source: {
-                                    uri: item?.profile_photo_1?.fileUrl,
-                                  },
-                                }
-                              : {})}
+            {communityLength >= 2 ? (
+              <VStack>
+                <HStack
+                  justifyContent={"space-between"}
+                  alignItems={"center"}
+                  mt={5}
+                >
+                  <VStack width={"50%"}>
+                    <AdminTypo.H3 color="textMaroonColor.400">
+                      {`${nonRegisteredUser?.length} `}
+                      {t("BENEFICIARY_STATUS_ENROLLED_IP_VERIFIED")}
+                    </AdminTypo.H3>
+                    <AdminTypo.H3 color="textMaroonColor.400">
+                      {t("LEARNERS")}
+                    </AdminTypo.H3>
+                  </VStack>
+                  <HStack>
+                    <Center>
+                      {nonRegisteredUser.length > 0 && (
+                        <Avatar.Group
+                          _avatar={{
+                            size: "lg",
+                          }}
+                          max={3}
+                        >
+                          {nonRegisteredUser?.map((item) => {
+                            return (
+                              <Avatar
+                                key={item}
+                                bg="red.500"
+                                {...(item?.profile_photo_1?.fileUrl
+                                  ? {
+                                      source: {
+                                        uri: item?.profile_photo_1?.fileUrl,
+                                      },
+                                    }
+                                  : {})}
+                              >
+                                {item?.program_beneficiaries[0]
+                                  ?.enrollment_first_name
+                                  ? item?.program_beneficiaries[0]?.enrollment_first_name?.substring(
+                                      0,
+                                      2
+                                    )
+                                  : "NA"}
+                              </Avatar>
+                            );
+                          })}
+                        </Avatar.Group>
+                      )}
+                    </Center>
+                  </HStack>
+                </HStack>
+                <VStack mt={5}>
+                  <VStack my={3} space={2}>
+                    {campList?.map((item) => {
+                      return (
+                        <Pressable
+                          key={item}
+                          onPress={() => {
+                            navigate(`/camps/${item?.id}`);
+                          }}
+                        >
+                          <HStack
+                            bg="white"
+                            p="2"
+                            my={2}
+                            shadow="FooterShadow"
+                            rounded="sm"
+                            space="1"
+                            alignItems={"center"}
+                            justifyContent={"space-between"}
                           >
-                            {item?.program_beneficiaries[0]
-                              ?.enrollment_first_name
-                              ? item?.program_beneficiaries[0]?.enrollment_first_name?.substring(
-                                  0,
-                                  2
-                                )
-                              : "NA"}
-                          </Avatar>
-                        );
-                      })}
-                    </Avatar.Group>
-                  )}
-                </Center>
-              </HStack>
-            </HStack>
-            <VStack mt={5}>
-              <VStack my={3} space={2}>
-                {campList?.map((item) => {
-                  return (
-                    <Pressable
-                      key={item}
-                      onPress={() => {
-                        navigate(`/camps/${item?.id}`);
-                      }}
-                    >
-                      <HStack
-                        bg="white"
-                        p="2"
-                        my={2}
-                        shadow="FooterShadow"
-                        rounded="sm"
-                        space="1"
-                        alignItems={"center"}
-                        justifyContent={"space-between"}
-                      >
-                        <VStack>
-                          <AdminTypo.H3>{item?.group?.name}</AdminTypo.H3>
-                          {item?.group?.description && (
-                            <Text>{item?.group?.description}</Text>
-                          )}
-                        </VStack>
-                        <HStack>
-                          <IconByName
-                            isDisabled
-                            name="ErrorWarningLineIcon"
-                            color="textMaroonColor.400"
-                            _icon={{ size: "20px" }}
-                          />
-                          <GetEnumValue
-                            t={t}
-                            enumType={"GROUPS_STATUS"}
-                            enumOptionValue={item?.group?.status}
-                            enumApiData={enumOptions}
-                            color="textMaroonColor.400"
-                            ml={2}
-                          />
-                        </HStack>
-                      </HStack>
-                    </Pressable>
-                  );
-                })}
-              </VStack>
-            </VStack>
+                            <VStack>
+                              <AdminTypo.H3>{item?.group?.name}</AdminTypo.H3>
+                              {item?.group?.description && (
+                                <Text>{item?.group?.description}</Text>
+                              )}
+                            </VStack>
+                            <HStack>
+                              <IconByName
+                                isDisabled
+                                name="ErrorWarningLineIcon"
+                                color="textMaroonColor.400"
+                                _icon={{ size: "20px" }}
+                              />
+                              <GetEnumValue
+                                t={t}
+                                enumType={"GROUPS_STATUS"}
+                                enumOptionValue={item?.group?.status}
+                                enumApiData={enumOptions}
+                                color="textMaroonColor.400"
+                                ml={2}
+                              />
+                            </HStack>
+                          </HStack>
+                        </Pressable>
+                      );
+                    })}
+                  </VStack>
+                </VStack>
 
-            {campList?.length !== 2 && (
-              <FrontEndTypo.Secondarybutton
-                onPress={() => {
-                  navigate(`/camps/new/learners`, { state: "camp" });
-                }}
+                {campList?.length !== 2 && (
+                  <FrontEndTypo.Secondarybutton
+                    onPress={() => {
+                      navigate(`/camps/new/learners`, { state: "camp" });
+                    }}
+                  >
+                    {campList?.length !== 0
+                      ? t("START_SECOND_CAMP_REGISTER")
+                      : t("START_FIRST_CAMP_REGISTER")}
+                  </FrontEndTypo.Secondarybutton>
+                )}
+                <Alert
+                  status="warning"
+                  alignItems={"start"}
+                  mb="3"
+                  mt="4"
+                  width={"100%"}
+                >
+                  <HStack alignItems="center" space="2" color>
+                    <Alert.Icon />
+                    <BodyMedium>{t("CAMP_WARNING")}</BodyMedium>
+                  </HStack>
+                </Alert>
+              </VStack>
+            ) : (
+              <Alert
+                status="warning"
+                alignItems={"start"}
+                mb="3"
+                mt="4"
+                width={"100%"}
               >
-                {campList?.length !== 0
-                  ? t("START_SECOND_CAMP_REGISTER")
-                  : t("START_FIRST_CAMP_REGISTER")}
-              </FrontEndTypo.Secondarybutton>
+                <HStack alignItems="center" space="2" color>
+                  <Alert.Icon />
+                  <BodyMedium>{t("COMMUNITY_min_error")}</BodyMedium>
+                </HStack>
+              </Alert>
             )}
-            <Alert
-              status="warning"
-              alignItems={"start"}
-              mb="3"
-              mt="4"
-              width={"100%"}
-            >
-              <HStack alignItems="center" space="2" color>
-                <Alert.Icon />
-                <BodyMedium>{t("CAMP_WARNING")}</BodyMedium>
-              </HStack>
-            </Alert>
           </VStack>
         ) : (
           <Alert
