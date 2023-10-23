39eee43e8895c51155c354dc0b6700764217a672
swapnilphalke03 <swapnil.phalke@tekditechnologies.com>
2023-10-23 11:19:24 +0530

    changes

--- apps/front-end/src/pages/admin/camps/View.js
+++ apps/front-end/src/pages/admin/camps/View.js
@@ -133,7 +133,7 @@ export default function View({ footerLinks }) {
           <IconByName
             size="sm"
             name="ArrowRightSLineIcon"
-            onPress={(e) => navigate(`/admin/campHome`)}
+            onPress={(e) => navigate(`/admin/camp`)}
           />
           <AdminTypo.H1
             color="textGreyColor.800"
