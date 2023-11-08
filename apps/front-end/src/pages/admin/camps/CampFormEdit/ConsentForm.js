8dbf3da16befc9d76bd6e3325c28156c00c22736
sonaligarud <sonali.garud@tekditechnologies.com>
2023-11-08 15:33:44 +0530

    added space

--- apps/front-end/src/pages/admin/camps/CampFormEdit/ConsentForm.js
+++ apps/front-end/src/pages/admin/camps/CampFormEdit/ConsentForm.js
@@ -140,6 +140,7 @@ export default function ConsentForm() {
                   {document?.document_id ? (
                     <HStack>
                       {t("VIEW")}
+
                       <IconByName
                         isDisabled
                         name="FileTextLineIcon"
