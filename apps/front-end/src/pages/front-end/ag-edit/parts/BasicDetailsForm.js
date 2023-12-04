a76c08025cfd34eed8a513a39a5d9ff433b0f517
Vivek Kasture <vivek_kasture@techjoomla.com>
2023-12-04 18:50:33 +0530

    Merge branch 'release-1.5.6' of github.com:tekdi/eg-website into release-1.5.6

--- apps/front-end/src/pages/front-end/ag-edit/parts/BasicDetailsForm.js
+++ apps/front-end/src/pages/front-end/ag-edit/parts/BasicDetailsForm.js
@@ -3,7 +3,6 @@ import Form from "@rjsf/core";
 import validator from "@rjsf/validator-ajv8";
 import schema1 from "./schema.js";
 import { Alert, Box, HStack } from "native-base";
-import useOnlineStatus from "@shiksha/common-lib";
 
 import {
   Layout,
@@ -215,9 +214,9 @@ export default function BasicDetailsForm({ id }) {
 
   const onSubmit = async (data) => {
     
-    // setIsDisable(true);
-    // await AgRegistryService.updateAg(formData, userId);
-    // navigate(`/beneficiary/${userId}/basicdetails`);
+    setIsDisable(true);
+    await AgRegistryService.updateAg(formData, userId);
+    navigate(`/beneficiary/${userId}/basicdetails`);
   };
 
   return (
