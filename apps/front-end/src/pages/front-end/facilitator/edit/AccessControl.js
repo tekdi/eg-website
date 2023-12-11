c3ce661c4808e77848beba6d085ac18d37199fc9
swapnilphalke03 <swapnil.phalke@tekditechnologies.com>
2023-12-11 13:03:32 +0530

    Edit access on request

--- apps/front-end/src/pages/front-end/facilitator/edit/AccessControl.js
+++ apps/front-end/src/pages/front-end/facilitator/edit/AccessControl.js
@@ -3,7 +3,7 @@ const accessControl = (schema, fields) => {
   for (const field of fields) {
     if (newSchema.properties?.[field]) {
       if (newSchema.properties.hasOwnProperty(field)) {
-        newSchema.properties[field].readOnly = "true";
+        newSchema.properties[field].readOnly = false;
       } else {
         const { readOnly, ...propData } = newSchema.properties[field] || {};
         newSchema.properties[field] = propData;
