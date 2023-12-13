4f413e7e47ade2bad2578160161c250f6ac5f4e1
swapnilphalke03 <swapnil.phalke@tekditechnologies.com>
2023-12-13 15:52:10 +0530

    Changes

--- apps/front-end/src/pages/admin/beneficiaries/AdminBeneficiariesListTable.js
+++ apps/front-end/src/pages/admin/beneficiaries/AdminBeneficiariesListTable.js
@@ -13,137 +13,140 @@ import DataTable from "react-data-table-component";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
-const columns = useCallback(
-  (t, navigate) => [
-    {
-      name: t("LEARNERS_ID"),
-      selector: (row) => row?.id,
-      wrap: true,
-      width: "95px",
-    },
-    {
-      name: t("LEARNERS_NAME"),
-      selector: (row) => (
-        <HStack>
-          <Pressable onPress={() => navigate(`/admin/beneficiary/${row?.id}`)}>
-            <HStack alignItems={"center"} space={2}>
-              {row?.profile_photo_1?.name ? (
-                <ImageView
-                  urlObject={row?.profile_photo_1}
-                  // alt="Alternate Text"
-                  width={"35px"}
-                  height={"35px"}
-                />
-              ) : (
-                <IconByName
-                  isDisabled
-                  name="AccountCircleLineIcon"
-                  color="gray.300"
-                  _icon={{ size: "35" }}
-                />
-              )}
-              {row?.program_beneficiaries?.status === "enrolled_ip_verified" ? (
-                <AdminTypo.H6 bold>
-                  {row?.program_beneficiaries?.enrollment_first_name + " "}
-                  {row?.program_beneficiaries?.enrollment_last_name
-                    ? row?.program_beneficiaries?.enrollment_last_name
-                    : ""}
-                </AdminTypo.H6>
-              ) : (
-                <AdminTypo.H6 bold>
-                  {row?.first_name + " "}
-                  {row?.last_name ? row?.last_name : ""}
-                </AdminTypo.H6>
-              )}
-            </HStack>
-          </Pressable>
-        </HStack>
-      ),
-      attr: "name",
-      wrap: true,
-      width: "250px",
-    },
-    {
-      name: t("LEARNERS_AGE"),
-      selector: (row) => {
-        if (row?.program_beneficiaries?.status === "enrolled_ip_verified") {
-          if (row?.program_beneficiaries?.enrollment_dob) {
-            return moment().diff(
-              row?.program_beneficiaries.enrollment_dob,
-              "years"
-            );
-          } else {
-            return "-";
-          }
-        } else {
-          if (row?.dob) {
-            return moment().diff(row?.dob, "years");
-          } else {
-            return "-";
-          }
-        }
-      },
-    },
-    {
-      name: t("PRERAK_ID"),
-      selector: (row) => row?.program_beneficiaries?.facilitator_id,
-    },
-    {
-      name: t("PRERAK_NAME"),
-      selector: (row) => {
-        const {
-          program_beneficiaries: {
-            facilitator_user: { first_name, last_name },
-          },
-        } = row;
-        return first_name || last_name
-          ? `${first_name} ${last_name || ""}`
-          : "-";
-      },
-      wrap: true,
-      width: "250px",
-    },
-    {
-      name: t("STATUS"),
-      selector: (row, index) => (
-        <Pressable onPress={() => navigate(`/admin/beneficiary/${row?.id}`)}>
-          <ChipStatus
-            key={index}
-            is_duplicate={row?.is_duplicate}
-            is_deactivated={row?.is_deactivated}
-            status={row?.program_beneficiaries?.status}
-          />
-        </Pressable>
-      ),
-
-      attr: "email",
-      wrap: true,
-      width: "250px",
-    },
-    {
-      name: t("ACTION"),
-      selector: (row) => (
-        <AdminTypo.Secondarybutton
-          my="3"
-          onPress={() => {
-            navigate(`/admin/beneficiary/${row?.id}`);
-          }}
-        >
-          {t("VIEW")}
-        </AdminTypo.Secondarybutton>
-      ),
-      wrap: true,
-    },
-  ],
-  [District, Block]
-);
-
 // Table component
 function Table({ filter, setFilter, paginationTotalRows, data, loading }) {
   const [beneficiaryStatus, setBeneficiaryStatus] = React.useState();
 
   const { t } = useTranslation();
   const navigate = useNavigate();
+
+  const columns = React.useCallback(
+    (t, navigate) => [
+      {
+        name: t("LEARNERS_ID"),
+        selector: (row) => row?.id,
+        wrap: true,
+        width: "95px",
+      },
+      {
+        name: t("LEARNERS_NAME"),
+        selector: (row) => (
+          <HStack>
+            <Pressable
+              onPress={() => navigate(`/admin/beneficiary/${row?.id}`)}
+            >
+              <HStack alignItems={"center"} space={2}>
+                {row?.profile_photo_1?.name ? (
+                  <ImageView
+                    urlObject={row?.profile_photo_1}
+                    // alt="Alternate Text"
+                    width={"35px"}
+                    height={"35px"}
+                  />
+                ) : (
+                  <IconByName
+                    isDisabled
+                    name="AccountCircleLineIcon"
+                    color="gray.300"
+                    _icon={{ size: "35" }}
+                  />
+                )}
+                {row?.program_beneficiaries?.status ===
+                "enrolled_ip_verified" ? (
+                  <AdminTypo.H6 bold>
+                    {row?.program_beneficiaries?.enrollment_first_name + " "}
+                    {row?.program_beneficiaries?.enrollment_last_name
+                      ? row?.program_beneficiaries?.enrollment_last_name
+                      : ""}
+                  </AdminTypo.H6>
+                ) : (
+                  <AdminTypo.H6 bold>
+                    {row?.first_name + " "}
+                    {row?.last_name ? row?.last_name : ""}
+                  </AdminTypo.H6>
+                )}
+              </HStack>
+            </Pressable>
+          </HStack>
+        ),
+        attr: "name",
+        wrap: true,
+        width: "250px",
+      },
+      {
+        name: t("LEARNERS_AGE"),
+        selector: (row) => {
+          if (row?.program_beneficiaries?.status === "enrolled_ip_verified") {
+            if (row?.program_beneficiaries?.enrollment_dob) {
+              return moment().diff(
+                row?.program_beneficiaries.enrollment_dob,
+                "years"
+              );
+            } else {
+              return "-";
+            }
+          } else {
+            if (row?.dob) {
+              return moment().diff(row?.dob, "years");
+            } else {
+              return "-";
+            }
+          }
+        },
+      },
+      {
+        name: t("PRERAK_ID"),
+        selector: (row) => row?.program_beneficiaries?.facilitator_id,
+      },
+      {
+        name: t("PRERAK_NAME"),
+        selector: (row) => {
+          const {
+            program_beneficiaries: {
+              facilitator_user: { first_name, last_name },
+            },
+          } = row;
+          return first_name || last_name
+            ? `${first_name} ${last_name || ""}`
+            : "-";
+        },
+        wrap: true,
+        width: "250px",
+      },
+      {
+        name: t("STATUS"),
+        selector: (row, index) => (
+          <Pressable onPress={() => navigate(`/admin/beneficiary/${row?.id}`)}>
+            <ChipStatus
+              key={index}
+              is_duplicate={row?.is_duplicate}
+              is_deactivated={row?.is_deactivated}
+              status={row?.program_beneficiaries?.status}
+            />
+          </Pressable>
+        ),
+
+        attr: "email",
+        wrap: true,
+        width: "250px",
+      },
+      {
+        name: t("ACTION"),
+        selector: (row) => (
+          <AdminTypo.Secondarybutton
+            my="3"
+            onPress={() => {
+              navigate(`/admin/beneficiary/${row?.id}`);
+            }}
+          >
+            {t("VIEW")}
+          </AdminTypo.Secondarybutton>
+        ),
+        wrap: true,
+      },
+    ],
+    [District, Block]
+  );
 
   const fetchEnumRegistry = React.useCallback(async () => {
     const result = await enumRegistryService.listOfEnum();
--- apps/front-end/src/pages/admin/facilitator/Table.js
+++ apps/front-end/src/pages/admin/facilitator/Table.js
@@ -30,150 +30,6 @@ const dropDown = (triggerProps, t) => {
   );
 };
 
-const columns = useCallback(
-  (t, navigate) => [
-    {
-      name: t("PRERAK_ID"),
-      selector: (row) => row?.id,
-      sortable: true,
-      attr: "id",
-      wrap: true,
-      width: "100px",
-    },
-    {
-      name: t("NAME"),
-      selector: (row) => (
-        <HStack>
-          <Pressable
-            style={{ flexDirection: "row", justifyContent: "space-between" }}
-            onPress={() => navigate(`/admin/facilitator/${row?.id}`)}
-          >
-            <HStack alignItems={"center"} space={2}>
-              {row?.profile_photo_1?.name ? (
-                <ImageView
-                  urlObject={row?.profile_photo_1}
-                  alt="Alternate Text"
-                  width={"35px"}
-                  height={"35px"}
-                />
-              ) : (
-                <IconByName
-                  isDisabled
-                  name="AccountCircleLineIcon"
-                  color="gray.300"
-                  _icon={{ size: "35" }}
-                />
-              )}
-              <AdminTypo.H6 bold>
-                {`${row?.first_name} ${row?.last_name || ""}`}
-              </AdminTypo.H6>
-            </HStack>
-          </Pressable>
-        </HStack>
-      ),
-      attr: "name",
-      wrap: "true",
-      width: "250px",
-    },
-    {
-      name: t("DISTRICT"),
-      selector: (row) => (row?.district ? row?.district : "-"),
-    },
-    {
-      name: t("OKYC_VERIFICATION"),
-      wrap: true,
-      selector: (row) =>
-        ["okyc_ip_verified"].includes(row?.aadhar_verified)
-          ? t("YES")
-          : t("NO"),
-    },
-    {
-      name: t("MOBILE_NUMBER"),
-      selector: (row) => row?.mobile,
-      attr: "mobile",
-      wrap: true,
-    },
-    {
-      name: t("STATUS"),
-      selector: (row) => (
-        <Pressable onPress={() => navigate(`/admin/facilitator/${row?.id}`)}>
-          <ChipStatus status={row?.program_faciltators?.status} />
-        </Pressable>
-      ),
-      wrap: true,
-      attr: "status",
-      width: "150px",
-    },
-    {
-      name: t("GENDER"),
-      selector: (row) => row?.gender,
-      attr: "gender",
-      width: "100px",
-    },
-    {
-      minWidth: "140px",
-      name: t("ACTION"),
-      selector: (row) => (
-        <Button.Group
-          isAttached
-          divider={<h3>|</h3>}
-          my="3"
-          size="sm"
-          h="10"
-          marginTop="8px"
-          borderRadius="full"
-          background="white"
-          shadow="BlueOutlineShadow"
-          borderWidth="1px"
-          borderColor="#084B82"
-          lineHeight={8}
-          _text={{
-            color: "blueText.400",
-            fontSize: "14px",
-            fontWeight: "700",
-          }}
-        >
-          <Button
-            background="white"
-            _text={{
-              color: "blueText.400",
-              fontSize: "14px",
-              fontWeight: "700",
-            }}
-            onPress={() => {
-              navigate(`/admin/facilitator/${row?.id}`);
-            }}
-          >
-            {t("VIEW")}
-          </Button>
-          <Button variant="outline">
-            <Menu
-              w="190"
-              placement="bottom right"
-              trigger={(triggerProps) => dropDown(triggerProps, t)}
-            >
-              <Menu.Item
-                onPress={() => {
-                  navigate(`/admin/facilitator/${row?.id}`);
-                }}
-              >
-                {t("VIEW")}
-              </Menu.Item>
-              <Menu.Item
-                onPress={() => {
-                  navigate(`/admin/Certification/${row?.id}`);
-                }}
-              >
-                {t("DOWNLOAD_CERTIFICATE")}
-              </Menu.Item>
-            </Menu>
-          </Button>
-        </Button.Group>
-      ),
-    },
-  ],
-  [District, Block]
-);
 // Table component
 function Table({
   filter,
@@ -187,6 +43,151 @@ function Table({
   const { t } = useTranslation();
 
   const navigate = useNavigate();
+
+  const columns = React.useCallback(
+    (t, navigate) => [
+      {
+        name: t("PRERAK_ID"),
+        selector: (row) => row?.id,
+        sortable: true,
+        attr: "id",
+        wrap: true,
+        width: "100px",
+      },
+      {
+        name: t("NAME"),
+        selector: (row) => (
+          <HStack>
+            <Pressable
+              style={{ flexDirection: "row", justifyContent: "space-between" }}
+              onPress={() => navigate(`/admin/facilitator/${row?.id}`)}
+            >
+              <HStack alignItems={"center"} space={2}>
+                {row?.profile_photo_1?.name ? (
+                  <ImageView
+                    urlObject={row?.profile_photo_1}
+                    alt="Alternate Text"
+                    width={"35px"}
+                    height={"35px"}
+                  />
+                ) : (
+                  <IconByName
+                    isDisabled
+                    name="AccountCircleLineIcon"
+                    color="gray.300"
+                    _icon={{ size: "35" }}
+                  />
+                )}
+                <AdminTypo.H6 bold>
+                  {`${row?.first_name} ${row?.last_name || ""}`}
+                </AdminTypo.H6>
+              </HStack>
+            </Pressable>
+          </HStack>
+        ),
+        attr: "name",
+        wrap: "true",
+        width: "250px",
+      },
+      {
+        name: t("DISTRICT"),
+        selector: (row) => (row?.district ? row?.district : "-"),
+      },
+      {
+        name: t("OKYC_VERIFICATION"),
+        wrap: true,
+        selector: (row) =>
+          ["okyc_ip_verified"].includes(row?.aadhar_verified)
+            ? t("YES")
+            : t("NO"),
+      },
+      {
+        name: t("MOBILE_NUMBER"),
+        selector: (row) => row?.mobile,
+        attr: "mobile",
+        wrap: true,
+      },
+      {
+        name: t("STATUS"),
+        selector: (row) => (
+          <Pressable onPress={() => navigate(`/admin/facilitator/${row?.id}`)}>
+            <ChipStatus status={row?.program_faciltators?.status} />
+          </Pressable>
+        ),
+        wrap: true,
+        attr: "status",
+        width: "150px",
+      },
+      {
+        name: t("GENDER"),
+        selector: (row) => row?.gender,
+        attr: "gender",
+        width: "100px",
+      },
+      {
+        minWidth: "140px",
+        name: t("ACTION"),
+        selector: (row) => (
+          <Button.Group
+            isAttached
+            divider={<h3>|</h3>}
+            my="3"
+            size="sm"
+            h="10"
+            marginTop="8px"
+            borderRadius="full"
+            background="white"
+            shadow="BlueOutlineShadow"
+            borderWidth="1px"
+            borderColor="#084B82"
+            lineHeight={8}
+            _text={{
+              color: "blueText.400",
+              fontSize: "14px",
+              fontWeight: "700",
+            }}
+          >
+            <Button
+              background="white"
+              _text={{
+                color: "blueText.400",
+                fontSize: "14px",
+                fontWeight: "700",
+              }}
+              onPress={() => {
+                navigate(`/admin/facilitator/${row?.id}`);
+              }}
+            >
+              {t("VIEW")}
+            </Button>
+            <Button variant="outline">
+              <Menu
+                w="190"
+                placement="bottom right"
+                trigger={(triggerProps) => dropDown(triggerProps, t)}
+              >
+                <Menu.Item
+                  onPress={() => {
+                    navigate(`/admin/facilitator/${row?.id}`);
+                  }}
+                >
+                  {t("VIEW")}
+                </Menu.Item>
+                <Menu.Item
+                  onPress={() => {
+                    navigate(`/admin/Certification/${row?.id}`);
+                  }}
+                >
+                  {t("DOWNLOAD_CERTIFICATE")}
+                </Menu.Item>
+              </Menu>
+            </Button>
+          </Button.Group>
+        ),
+      },
+    ],
+    [District, Block]
+  );
 
   const handleRowClick = React.useCallback(
     (row) => {
