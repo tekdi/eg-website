735507621091ef85df2810e35652c14f8e59230e
Sagar Takle <sagarkoshti1990@gmail.com>
2023-12-14 22:03:28 +0530

    fix:event pagination, camp exicution and certification button

--- apps/front-end/src/pages/front-end/BenificiaryProfilePhoto.js
+++ apps/front-end/src/pages/front-end/BenificiaryProfilePhoto.js
@@ -103,11 +103,7 @@ export default function BenificiaryProfilePhoto() {
               onChange={(e) => console.log(e)}
             />
           }
-          {errors?.fileSize ? (
-            <H2 color="red.400">{errors?.fileSize}</H2>
-          ) : (
-            <React.Fragment />
-          )}
+          {errors?.fileSize && <H2 color="red.400">{errors?.fileSize}</H2>}
           <FrontEndTypo.Primarybutton
             isLoading={loading}
             p="4"
