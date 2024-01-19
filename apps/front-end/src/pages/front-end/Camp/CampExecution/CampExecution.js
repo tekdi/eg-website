e92e44ebae252c94e295c7c12fb98ec7c3fc2b0d
Sagar Takle <sagarkoshti1990@gmail.com>
2024-01-19 18:02:05 +0530

    fix: issue mood list for start camp

--- apps/front-end/src/pages/front-end/Camp/CampExecution/CampExecution.js
+++ apps/front-end/src/pages/front-end/Camp/CampExecution/CampExecution.js
@@ -90,7 +90,7 @@ export default function CampExecution({ footerLinks, setAlert }) {
       }));
       setMoodList(moodListWithImages);
     }
-  }, []);
+  }, [cameraFile, cameraUrl]);
 
   React.useEffect(async () => {
     enumData();
