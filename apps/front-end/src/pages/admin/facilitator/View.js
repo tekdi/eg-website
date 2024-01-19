5f98b9851843c3d01b27d881dde742f5228816de
chaitanyakole <124124878+chaitanyakole@users.noreply.github.com>
2024-01-19 17:47:35 +0530

    Bug #212150, Bug #212145; [WEB ADMIN] : Stuck Loading Issue After Prerak Password Reset; [WEB ADMIN] : Aadhar Number Disappearing Issue in Prerak Update (#1023)
    * Bug #212150
    
    * changes in code
    
    * changes in code

--- apps/front-end/src/pages/admin/facilitator/View.js
+++ apps/front-end/src/pages/admin/facilitator/View.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect, useState, memo } from "react";
 import {
   IconByName,
   AdminLayout as Layout,
@@ -126,41 +126,42 @@ export default function FacilitatorView({ footerLinks }) {
   const toast = useToast();
 
   const { id } = useParams();
-  const [data, setData] = React.useState();
-  const [modalVisible, setModalVisible] = React.useState(false);
-  const [adhaarModalVisible, setAdhaarModalVisible] = React.useState(false);
-  const [aadhaarValue, setAadhaarValue] = React.useState();
-  const [duplicateUserList, setDuplicateUserList] = React.useState();
-  const [aadhaarerror, setAadhaarError] = React.useState();
-  const [credentials, setCredentials] = React.useState();
-  const [errors, setErrors] = React.useState({});
-  const [showPassword, setShowPassword] = React.useState(false);
-  const [confirmPassword, setConfirmPassword] = React.useState(false);
-  const [qualifications, setQualifications] = React.useState([]);
-  const [editModal, setEditModal] = React.useState(false);
-  const [editData, setEditData] = React.useState();
-  const [fieldCheck, setFieldCheck] = React.useState();
-  const [isButtonLoading, setIsButtonLoading] = React.useState(false);
+  const [data, setData] = useState();
+  const [modalVisible, setModalVisible] = useState(false);
+  const [adhaarModalVisible, setAdhaarModalVisible] = useState(false);
+  const [aadhaarValue, setAadhaarValue] = useState();
+  const [duplicateUserList, setDuplicateUserList] = useState();
+  const [aadhaarerror, setAadhaarError] = useState();
+  const [credentials, setCredentials] = useState();
+  const [errors, setErrors] = useState({});
+  const [showPassword, setShowPassword] = useState(false);
+  const [confirmPassword, setConfirmPassword] = useState(false);
+  const [qualifications, setQualifications] = useState([]);
+  const [editModal, setEditModal] = useState(false);
+  const [editData, setEditData] = useState();
+  const [fieldCheck, setFieldCheck] = useState();
+  const [isButtonLoading, setIsButtonLoading] = useState(false);
 
-  const togglePasswordVisibility = React.useCallback(() => {
+  const togglePasswordVisibility = useCallback(() => {
     setShowPassword((prevShowPassword) => !prevShowPassword);
   }, []);
 
-  const toggleConfirmPasswordVisibility = React.useCallback(() => {
+  const toggleConfirmPasswordVisibility = useCallback(() => {
     setConfirmPassword((prevConfirmPassword) => !prevConfirmPassword);
   }, []);
 
-  const openModal = React.useCallback(() => {
+  const openModal = useCallback(() => {
     setEditModal(true);
   }, []);
 
   const navigate = useNavigate();
 
-const profileDetails = React.useCallback(async () => {
+  const profileDetails = useCallback(async () => {
     const result = await facilitatorRegistryService.getOne({ id });
     setData(result);
     setAadhaarValue(result?.aadhar_no);
-    const qualificationList = await facilitatorRegistryService.getQualificationAll();
+    const qualificationList =
+      await facilitatorRegistryService.getQualificationAll();
     const qual = JSON.parse(result?.program_faciltators?.qualification_ids);
 
     if (qual?.length > 0) {
@@ -173,11 +174,11 @@ const profileDetails = React.useCallback(async () => {
     }
   }, [id]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     profileDetails();
   }, [profileDetails]);
 
-  React.useEffect(async () => {
+  useEffect(async () => {
     const obj = {
       edit_req_for_context: "users",
       edit_req_for_context_id: id,
@@ -217,7 +218,7 @@ const profileDetails = React.useCallback(async () => {
 
   const showData = (item) => item || "-";
 
-  const validate = React.useCallback(() => {
+  const validate = useCallback(() => {
     let arr = {};
     const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
 
@@ -271,6 +272,7 @@ const profileDetails = React.useCallback(async () => {
             description: resetPassword?.message,
           });
           setModalVisible(false);
+          setIsButtonLoading(false);
           return { status: true };
         } else if (resetPassword.success === false) {
           setIsButtonLoading(false);
@@ -297,7 +299,7 @@ const profileDetails = React.useCallback(async () => {
   }
 
   const handleAadhaarUpdate = (event) => {
-    const inputValue = event.target.value;
+    const inputValue = event?.target?.value;
     const numericValue = inputValue.replace(/\D/g, "");
     const maxLength = 12;
     const truncatedValue = numericValue.slice(0, maxLength);
@@ -313,7 +315,7 @@ const profileDetails = React.useCallback(async () => {
     const result = await facilitatorRegistryService.updateAadhaarNumber(
       aadhaar_no
     );
-    if (aadhaarValue.length < 12) {
+    if (aadhaarValue?.length < 12) {
       setAadhaarError("AADHAAR_SHOULD_BE_12_DIGIT_VALID_NUMBER");
       setIsButtonLoading(false);
     } else if (!result?.success) {
@@ -324,8 +326,8 @@ const profileDetails = React.useCallback(async () => {
       setData({ ...data, aadhar_no: aadhaarValue });
       setAdhaarModalVisible(false);
     }
+    setIsButtonLoading(false);
   };
-
   return (
     <Layout _sidebar={footerLinks}>
       <HStack>
@@ -424,7 +426,6 @@ const profileDetails = React.useCallback(async () => {
           </HStack>
 
           <HStack alignItems={Center} space="9">
-           
             <VStack space={4}>
               <AdminTypo.Secondarybutton
                 onPress={() => {
@@ -714,6 +715,7 @@ const profileDetails = React.useCallback(async () => {
                       color="textMaroonColor.400"
                       name="PencilLineIcon"
                       onPress={(e) => {
+                        setAadhaarValue(data?.aadhar_no);
                         setAdhaarModalVisible(!adhaarModalVisible);
                       }}
                     />
@@ -1075,27 +1077,25 @@ const profileDetails = React.useCallback(async () => {
   );
 }
 
-const SelectAllCheckBox = React.memo(({ fields, title, setFieldCheck, fieldCheck }) => {
+const SelectAllCheckBox = memo(
+  ({ fields, title, setFieldCheck, fieldCheck }) => {
+    const handleChange = useCallback(
+      (e) => {
+        if (!e) {
+          const checkedFields = fieldCheck?.filter(
+            (field) => !fields?.includes(field)
+          );
+          setFieldCheck(checkedFields);
+        } else {
+          const checkedFields = fieldCheck?.filter(
+            (field) => !fields?.includes(field)
+          );
+          setFieldCheck([...checkedFields, ...fields]);
+        }
+      },
+      [fields, fieldCheck, setFieldCheck]
+    );
 
-  const handleChange = React.useCallback((e) => {
-    if (!e) {
-      const checkedFields = fieldCheck?.filter(
-        (field) => !fields?.includes(field)
-      );
-      setFieldCheck(checkedFields);
-    } else {
-      const checkedFields = fieldCheck?.filter(
-        (field) => !fields?.includes(field)
-      );
-      setFieldCheck([...checkedFields, ...fields]);
-    }
-  }, [fields, fieldCheck, setFieldCheck]);
-
-  return (
-    <Checkbox
-      onChange={handleChange}
-    >
-      {title}
-    </Checkbox>
-  );
-});
+    return <Checkbox onChange={handleChange}>{title}</Checkbox>;
+  }
+);
