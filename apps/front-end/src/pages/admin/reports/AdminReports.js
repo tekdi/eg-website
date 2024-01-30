293d8873188f8e8490ea2537d422b20ee8d295cb
swapnilphalke03 <swapnil.phalke@tekditechnologies.com>
2024-01-30 17:03:01 +0530

    Changes

--- apps/front-end/src/pages/admin/reports/AdminReports.js
+++ apps/front-end/src/pages/admin/reports/AdminReports.js
@@ -26,7 +26,7 @@ export default function AdminReports({ footerLinks }) {
         <iframe
           title="reports"
           src={data}
-          border="0"
+          frameBorder="0"
           width="100%"
           height="900"
         />
