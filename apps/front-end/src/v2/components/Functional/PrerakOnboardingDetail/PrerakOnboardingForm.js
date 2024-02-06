a06b2237d0fed38b01fa6397f9f273cf1d489c19
chaitanya <chaitanya.kole@tekditechnologies.com>
2024-02-06 16:11:50 +0530

    Bug #213364

--- apps/front-end/src/v2/components/Functional/PrerakOnboardingDetail/PrerakOnboardingForm.js
+++ apps/front-end/src/v2/components/Functional/PrerakOnboardingDetail/PrerakOnboardingForm.js
@@ -911,6 +911,7 @@ export default function PrerakOnboardingForm({
   if (page === "upload") {
     return (
       <PhotoUpload
+        key={facilitator}
         {...{
           formData,
           cameraFile,
