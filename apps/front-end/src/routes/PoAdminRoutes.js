479ce4473a4e6aa0d2de500e9ea165e3f33b0224
chaitanya <chaitanya.kole@tekditechnologies.com>
2024-03-06 16:19:33 +0530

    Task #214634 [WEB-ADMIN] Show Prerak listing for Program owner

--- apps/front-end/src/routes/PoAdminRoutes.js
+++ apps/front-end/src/routes/PoAdminRoutes.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy } from "react";
 
 const NotFound = React.lazy(() => import("pages/NotFound"));
 const PoAdminHome = React.lazy(() => import("pages/admin/PoAdmin/PoAdminHome"));
@@ -7,8 +7,40 @@ const PoDuplicateView = React.lazy(() =>
 );
 const FileView = React.lazy(() => import("pages/FileView"));
 const Assessment = React.lazy(() => import("component/Assessment"));
+const PrerakListing = lazy(() => import("pages/admin/PoAdmin/Prerak/List"));
+// const CampListing = lazy(() => import("pages/admin/PoAdmin/Camp/List"));
+// const LearnerListing = lazy(() => import("pages/admin/PoAdmin/Learner/List"));
+// const CampViewDetails = lazy(() => import("pages/admin/PoAdmin/Camp/View"));
+// const LearnerViewDetails = lazy(() =>
+//   import("pages/admin/PoAdmin/Learner/View")
+// );
+// const PrerakViewDetails = lazy(() => import("pages/admin/PoAdmin/Prerak/View"));
 
 export default [
+  {
+    path: "/poadmin/facilitator",
+    component: PrerakListing,
+  },
+  // {
+  //   path: "/poadmin/facilitator/:id",
+  //   component: PrerakViewDetails,
+  // },
+  // {
+  //   path: "/poadmin/camps",
+  //   component: CampListing,
+  // },
+  // {
+  //   path: "/poadmin/camps/:id",
+  //   component: CampViewDetails,
+  // },
+  // {
+  //   path: "/poadmin/learner",
+  //   component: LearnerListing,
+  // },
+  // {
+  //   path: "/poadmin/learner/:id",
+  //   component: LearnerViewDetails,
+  // },
   { path: "/poadmin", component: PoAdminHome },
   {
     path: "/file/:id/view",
@@ -19,11 +51,9 @@ export default [
     component: PoDuplicateView,
   },
   { path: "/", component: PoAdminHome },
-
   {
     path: "/assessment/:context/:context_id/:do_id",
     component: Assessment,
   },
-
   { path: "*", component: NotFound },
 ];
