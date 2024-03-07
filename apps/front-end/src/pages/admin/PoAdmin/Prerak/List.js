6d66f934b7e07d2ad2073d45e22c6c00c4188d66
chaitanya <chaitanya.kole@tekditechnologies.com>
2024-03-07 12:25:04 +0530

    changes in code

--- apps/front-end/src/pages/admin/PoAdmin/Prerak/List.js
+++ apps/front-end/src/pages/admin/PoAdmin/Prerak/List.js
@@ -122,12 +122,11 @@ function PrerakList({ userTokenInfo }) {
 
   useEffect(() => {
     const fetchData = async () => {
-      // const programResult = await getSelectedProgramId();
-      // console.log({ programResult });
-      // let academic_Id = await getSelectedAcademicYear();
-      // setAcademicYear(academic_Id);
-      // setProgram(programResult);
-      let name = "RAJASTHAN";
+      const programResult = await getSelectedProgramId();
+      let academic_Id = await getSelectedAcademicYear();
+      setAcademicYear(academic_Id);
+      setProgram(programResult);
+      let name = programResult?.program?.state?.state_name;
       const getDistricts = await geolocationRegistryService.getDistricts({
         name,
       });
