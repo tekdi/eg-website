608be4e8beb8d51b8fdddc299a7d2fb2f1effb68
rahulg1254 <127941244+rahulg1254@users.noreply.github.com>
2024-03-13 18:24:13 +0530

    Task #215106 : 2.0 Updated serviceworkerRegistration to show popup and alert for the updates (#1217)
    * updated the label for service worker test
    
    * service worker test on dev
    
    * test 2 service worker
    
    * service worker issue

--- apps/front-end/src/bootstrap.js
+++ apps/front-end/src/bootstrap.js
@@ -37,14 +37,3 @@ if ("serviceWorker" in navigator) {
 
 //workbox
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('Service Worker registered:', registration);
-      })
-      .catch(error => {
-        console.error('Service Worker registration failed:', error);
-      });
-  });
-}
