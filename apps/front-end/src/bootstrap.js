7392a89f68f3dfdab06f1595856f5ee3031047b9
rahul_g <rahul_g@tekditechnologies.com>
2024-03-18 14:38:07 +0530

    added new changes to service worker file and index.js

--- apps/front-end/src/bootstrap.js
+++ apps/front-end/src/bootstrap.js
@@ -21,19 +21,3 @@ serviceWorkerRegistration.register();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-//cache clear 1.5.13
-
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/service-worker.js")
-    .then((registration) => {
-      console.log("Service Worker registered with scope:", registration.scope);
-    })
-    .catch((error) => {
-      console.error("Service Worker registration failed:", error);
-    });
-}
-
-//workbox
-
