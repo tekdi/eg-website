46e3c80a72ab2cd44be4b743bbd00081c6e6f183
Your Alfaj2302 <alfaj.shaikh@tekditechnologies.com>
2024-05-25 15:01:08 +0530

    Learner Branding Changes

--- apps/front-end/src/v2/views/Beneficiary/BenificiaryProfileView/BenificiaryProfileView.js
+++ apps/front-end/src/v2/views/Beneficiary/BenificiaryProfileView/BenificiaryProfileView.js
@@ -1,37 +1,37 @@
-import React, { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
 import {
-  HStack,
-  VStack,
-  Box,
-  Progress,
-  Divider,
-  Actionsheet,
-  Alert,
-  ScrollView,
-  Stack,
-} from "native-base";
-import {
-  FrontEndTypo,
-  IconByName,
-  Layout,
-  benificiaryRegistoryService,
-  enumRegistryService,
-  t,
-  ImageView,
   BodyMedium,
   CustomRadio,
+  FrontEndTypo,
+  IconByName,
+  ImageView,
+  Layout,
   arrList,
-  objProps,
-  setSelectedProgramId,
-  getOnboardingMobile,
-  setSelectedAcademicYear,
-  getSelectedProgramId,
+  benificiaryRegistoryService,
+  enumRegistryService,
   facilitatorRegistryService,
+  getOnboardingMobile,
+  getSelectedProgramId,
+  objProps,
+  setSelectedAcademicYear,
+  setSelectedProgramId,
+  t,
 } from "@shiksha/common-lib";
 import { ChipStatus } from "component/BeneficiaryStatus";
 import Clipboard from "component/Clipboard";
-import moment from "moment";
+import {
+  Actionsheet,
+  Alert,
+  Box,
+  Divider,
+  HStack,
+  Progress,
+  ScrollView,
+  Stack,
+  VStack,
+} from "native-base";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { getIpUserInfo, setIpUserInfo } from "v2/utils/SyncHelper/SyncHelper";
 
 export default function BenificiaryProfileView(props, userTokenInfo) {
   const [isOpenDropOut, setIsOpenDropOut] = React.useState(false);
@@ -51,7 +51,262 @@ export default function BenificiaryProfileView(props, userTokenInfo) {
   const [alert, setAlert] = React.useState();
   const [isDisable, setIsDisable] = React.useState(false);
   const navigate = useNavigate();
-  const [isDisableOpportunity, setIsDisableOpportunity] = React.useState(false);
+
+  // PROFILE DATA IMPORTS
+  const [facilitator, setFacilitator] = useState({ notLoaded: true });
+  const fa_id = localStorage.getItem("id");
+  const [countLoad, setCountLoad] = useState(0);
+  const [progress, setProgress] = useState(0);
+  const [cohortData, setCohortData] = useState(null);
+  const [programData, setProgramData] = useState(null);
+  const [isUserRegisterExist, setIsUserRegisterExist] = useState(false);
+  const [selectedCohortData, setSelectedCohortData] = useState(null);
+  const [selectedProgramData, setSelectedProgramData] = useState(null);
+  const [selectCohortForm, setSelectCohortForm] = useState(false);
+  const [academicYear, setAcademicYear] = useState(null);
+  const [academicData, setAcademicData] = useState([]);
+  const [isTodayAttendace, setIsTodayAttendace] = useState();
+  const [isOnline, setIsOnline] = useState(
+    window ? window.navigator.onLine : false
+  );
+
+  const saveDataToIndexedDB = async () => {
+    const obj = {
+      edit_req_for_context: "users",
+      edit_req_for_context_id: id,
+    };
+    try {
+      const [ListOfEnum, qualification, editRequest] = await Promise.all([
+        enumRegistryService.listOfEnum(),
+        enumRegistryService.getQualificationAll(),
+        facilitatorRegistryService.getEditRequests(obj),
+        // enumRegistryService.userInfo(),
+      ]);
+      const currentTime = moment().toString();
+      await Promise.all([
+        setIndexedDBItem("enums", ListOfEnum.data),
+        setIndexedDBItem("qualification", qualification),
+        setIndexedDBItem("lastFetchTime", currentTime),
+        setIndexedDBItem("editRequest", editRequest),
+      ]);
+    } catch (error) {
+      console.error("Error saving data to IndexedDB:", error);
+    }
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      // ...async operation
+      if (countLoad == 0) {
+        setCountLoad(1);
+      }
+      if (countLoad == 1) {
+        //do page load first operation
+        //get user info
+        if (userTokenInfo) {
+          const IpUserInfo = await getIpUserInfo(fa_id);
+          let ipUserData = IpUserInfo;
+          if (isOnline && !IpUserInfo) {
+            ipUserData = await setIpUserInfo(fa_id);
+          }
+
+          setFacilitator(ipUserData);
+        }
+        setLoading(false);
+        //end do page load first operation
+        setCountLoad(2);
+      } else if (countLoad == 2) {
+        setCountLoad(3);
+      }
+    }
+    fetchData();
+  }, [countLoad]);
+
+  useEffect(() => {
+    const fetchdata = async () => {
+      const programId = await getSelectedProgramId();
+      if (programId) {
+        try {
+          const c_data =
+            await facilitatorRegistryService.getPrerakCertificateDetails({
+              id: fa_id,
+            });
+          const data =
+            c_data?.data?.filter(
+              (eventItem) =>
+                eventItem?.params?.do_id?.length &&
+                eventItem?.lms_test_tracking?.length < 1
+            )?.[0] || {};
+          if (data) {
+            setIsTodayAttendace(
+              data?.attendances.filter(
+                (attendance) =>
+                  attendance.user_id == fa_id &&
+                  attendance.status == "present" &&
+                  data.end_date ==
+                    moment(attendance.date_time).format("YYYY-MM-DD")
+              )
+            );
+
+            setCertificateData(data);
+            if (data?.lms_test_tracking?.length > 0) {
+              setLmsDetails(data?.lms_test_tracking?.[0]);
+            }
+            const dataDay = moment.utc(data?.end_date).isSame(moment(), "day");
+            const format = "HH:mm:ss";
+            const time = moment(moment().format(format), format);
+            const beforeTime = moment.utc(data?.start_time, format).local();
+            const afterTime = moment.utc(data?.end_time, format).local();
+            if (time?.isBetween(beforeTime, afterTime) && dataDay) {
+              setIsEventActive(true);
+            }
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
+    fetchdata();
+  }, [selectedCohortData]);
+
+  useEffect(() => {
+    async function fetchData() {
+      // ...async operations
+      if (academicYear != null) {
+        //get cohort id and store in localstorage
+        const user_cohort_id = academicYear;
+        const cohort_data = await facilitatorRegistryService.getCohort({
+          cohortId: user_cohort_id,
+        });
+        setSelectedCohortData(cohort_data);
+        await setSelectedAcademicYear(cohort_data);
+      }
+    }
+    fetchData();
+  }, [academicYear]);
+
+  useEffect(() => {
+    async function fetchData() {
+      if (!facilitator?.notLoaded === true) {
+        // ...async operations
+        const res = objProps(facilitator);
+        setProgress(
+          arrList(
+            {
+              ...res,
+              qua_name: facilitator?.qualifications?.qualification_master?.name,
+            },
+            [
+              "device_ownership",
+              "mobile",
+              "device_type",
+              "gender",
+              "marital_status",
+              "social_category",
+              "name",
+              "contact_number",
+              "availability",
+              "aadhar_no",
+              "aadhaar_verification_mode",
+              "aadhar_verified",
+              "qualification_ids",
+              "qua_name",
+            ]
+          )
+        );
+        //check exist user registered
+        try {
+          let onboardingURLData = await getOnboardingURLData();
+          setCohortData(onboardingURLData?.cohortData);
+          setProgramData(onboardingURLData?.programData);
+          //get program id and store in localstorage
+
+          const user_program_id = facilitator?.program_faciltators?.program_id;
+          const program_data = await facilitatorRegistryService.getProgram({
+            programId: user_program_id,
+          });
+          setSelectedProgramData(program_data[0]);
+          await setSelectedProgramId(program_data[0]);
+          //check mobile number with localstorage mobile no
+          let mobile_no = facilitator?.mobile;
+          let mobile_no_onboarding = await getOnboardingMobile();
+          if (
+            mobile_no != null &&
+            mobile_no_onboarding != null &&
+            mobile_no == mobile_no_onboarding &&
+            onboardingURLData?.cohortData
+          ) {
+            //get cohort id and store in localstorage
+            const user_cohort_id =
+              onboardingURLData?.cohortData?.academic_year_id;
+            const cohort_data = await facilitatorRegistryService.getCohort({
+              cohortId: user_cohort_id,
+            });
+            setSelectedCohortData(cohort_data);
+            await setSelectedAcademicYear(cohort_data);
+            localStorage.setItem("loadCohort", "yes");
+            setIsUserRegisterExist(true);
+          } else {
+            setIsUserRegisterExist(false);
+            await showSelectCohort();
+          }
+        } catch (e) {}
+      }
+    }
+    fetchData();
+  }, [facilitator]);
+
+  const showSelectCohort = async () => {
+    let loadCohort = null;
+    try {
+      loadCohort = localStorage.getItem("loadCohort");
+    } catch (e) {}
+    if (loadCohort == null || loadCohort == "no") {
+      const user_cohort_list =
+        await facilitatorRegistryService.GetFacilatorCohortList();
+      let stored_response = await setSelectedAcademicYear(
+        user_cohort_list?.data[0]
+      );
+      setAcademicData(user_cohort_list?.data);
+      setAcademicYear(user_cohort_list?.data[0]?.academic_year_id);
+      localStorage.setItem("loadCohort", "yes");
+      if (user_cohort_list?.data.length == 1) {
+        setSelectCohortForm(false);
+        await checkDataToIndex();
+        await checkUserToIndex();
+      } else {
+        setSelectCohortForm(true);
+      }
+    }
+  };
+  const checkDataToIndex = async () => {
+    // Online Data Fetch Time Interval
+    const timeInterval = 30;
+    const enums = await getIndexedDBItem("enums");
+    const qualification = await getIndexedDBItem("qualification");
+    const lastFetchTime = await getIndexedDBItem("lastFetchTime");
+    const editRequest = await getIndexedDBItem("editRequest");
+    let timeExpired = false;
+    if (lastFetchTime) {
+      const timeDiff = moment
+        .duration(moment().diff(lastFetchTime))
+        .asMinutes();
+      if (timeDiff >= timeInterval) {
+        timeExpired = true;
+      }
+    }
+    if (
+      isOnline &&
+      (!enums ||
+        !qualification ||
+        !editRequest ||
+        timeExpired ||
+        !lastFetchTime ||
+        editRequest?.status === 400)
+    ) {
+      await saveDataToIndexedDB();
+    }
+  };
 
   React.useEffect(async () => {
     const result = await enumRegistryService.listOfEnum();
@@ -120,52 +375,8 @@ export default function BenificiaryProfileView(props, userTokenInfo) {
   };
   React.useEffect(async () => {
     const result = await benificiaryRegistoryService.getOne(id);
-    const Address = [
-      result?.result?.state,
-      result?.result?.district,
-      result?.result?.block,
-      result?.result?.village,
-      result?.result?.grampanchayat,
-    ]
-      .filter((e) => e)
-      .join(", ");
-    let fullName = [
-      result?.result?.program_beneficiaries?.enrollment_first_name,
-      result?.result?.program_beneficiaries?.enrollment_middle_name,
-      result?.result?.program_beneficiaries?.enrollment_last_name,
-    ]
-      .filter((e) => e)
-      .join(" ");
-    const userDetails = {
-      "Student Name": fullName,
-      name: fullName,
-      email:
-        result?.result?.email_id || `${result?.result?.first_name}@gmail.com`,
-      "Date Of Birth": result?.result?.dob,
-      birth_date: result?.result?.dob,
-      "mobile number": result?.result?.mobile,
-      phone: result?.result?.mobile,
-      contact: result?.result?.mobile,
-      Address,
-      createdAt: moment().format("YYYY-MM-DD HH:mm"),
-      user_id: id,
-    };
-    localStorage.setItem("userData", JSON.stringify(userDetails));
     setBenificiary(result?.result);
-    const orgResult = await benificiaryRegistoryService.getOrganisation({
-      id: userTokenInfo?.authUser?.program_faciltators?.parent_ip,
-    });
-    if (
-      ["enrolled_ip_verified", "registered_in_camp", "10th_passed"].includes(
-        result?.result?.program_beneficiaries?.status
-      ) &&
-      orgResult?.data?.name.toLowerCase() == "tekdi"
-    ) {
-      setIsDisableOpportunity(true);
-    } else {
-      setIsDisableOpportunity(false);
-    }
-    setloading(false);
+    setLoading(false);
   }, [reactivateReasonValue, reasonValue]);
 
   function renderDropoutButton() {
@@ -384,11 +595,6 @@ export default function BenificiaryProfileView(props, userTokenInfo) {
                 is_deactivated={benificiary?.is_deactivated}
                 rounded={"sm"}
               />
-              {isDisableOpportunity && (
-                <FrontEndTypo.Primarybutton onPress={(e) => navigate("/onest")}>
-                  {t("OPPORTUNITY")}
-                </FrontEndTypo.Primarybutton>
-              )}
             </VStack>
             {(benificiary?.program_beneficiaries?.status == "dropout" ||
               benificiary?.program_beneficiaries?.status == "rejected") && (
