9103836b242b228fffd8f23fe9691aef3c8efaae
chaitanya <chaitanya.kole@tekditechnologies.com>
2024-05-29 16:33:57 +0530

    Po-report

---
+++ apps/front-end/src/pages/admin/PoAdmin/IP/Report/ReportJson.js
@@ -0,0 +1,30 @@
+export const report = {
+  "ip-id-10944": {
+    id: 10944,
+    "prerak-report-1-1":
+      "https://egmb.tekdinext.com/public/question/0c4c0353-f2b8-4ba1-a2f0-720605e73a83",
+
+    "learner-report-1-1":
+      "https://egmb.tekdinext.com/public/question/8c692fc0-e69a-4411-ad32-c98635fb7d73",
+
+    "camp-report-1-1":
+      "https://egmb.tekdinext.com/public/question/8183f295-56b6-47d7-a2dd-0252c9b01e02",
+
+    "prerak-report-1-2":
+      "https://egmb.tekdinext.com/public/question/0c4c0353-f2b8-4ba1-a2f0-720605e73a83",
+
+    "learner-report-1-2":
+      "https://egmb.tekdinext.com/public/question/8c692fc0-e69a-4411-ad32-c98635fb7d73",
+
+    "camp-report-1-2":
+      "https://egmb.tekdinext.com/public/question/8183f295-56b6-47d7-a2dd-0252c9b01e02",
+
+    "prerak-report-3-2":
+      "https://egmb.tekdinext.com/public/question/36ec47f2-f43b-476d-a6c3-d5455455e3a2",
+
+    "learner-report-3-2":
+      "https://egmb.tekdinext.com/public/question/2851ffaa-d571-4f8f-bee0-93cca6907b26",
+
+    "camp-report-3-2": "",
+  },
+};
