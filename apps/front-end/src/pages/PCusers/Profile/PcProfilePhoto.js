bb16de759135a186cf891ea2a9e6ef899b46f585
snehalsabade <snehal.sabade@tekditechnologies.com>
2024-07-08 14:24:40 +0530

    Bug#222614: [WEB UI] : PC Profile Photo | Incorrect redirection happening

--- apps/front-end/src/pages/PCusers/Profile/PcProfilePhoto.js
+++ apps/front-end/src/pages/PCusers/Profile/PcProfilePhoto.js
@@ -71,7 +71,7 @@ export default function PcProfilePhoto() {
           <FrontEndTypo.Secondarybutton
             p="4"
             mt="4"
-            onPress={() => navigate(`/beneficiary/${id}/basicdetails`)}
+            onPress={() => navigate(`/profile`)}
           >
             {t("SAVE_AND_PROFILE")}
           </FrontEndTypo.Secondarybutton>
