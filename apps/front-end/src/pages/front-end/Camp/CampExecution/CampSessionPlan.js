32a0a06782f0c7c2b5e52218b479dc93736ead52
nawaz shaik <shariefnawaz1995@gmail.com>
2024-07-09 10:59:03 +0530

    Unused varialbles removed from the CmpSessionPlan file

--- apps/front-end/src/pages/front-end/Camp/CampExecution/CampSessionPlan.js
+++ apps/front-end/src/pages/front-end/Camp/CampExecution/CampSessionPlan.js
@@ -1,5 +1,5 @@
-import { CardComponent, FrontEndTypo, IconByName } from "@shiksha/common-lib";
-import { Alert, HStack, Image, Pressable, VStack } from "native-base";
+import { CardComponent, FrontEndTypo } from "@shiksha/common-lib";
+import { HStack, Image, Pressable } from "native-base";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 
@@ -39,29 +39,22 @@ export const CampSessionPlan = ({ button_name, id }) => {
   }
 
   return (
-    <VStack space={4}>
-      <Alert status="warning" py="2px" px="2" flexDirection="row" gap="2">
-        <Alert.Icon size="3" />
-        <FrontEndTypo.H4>{t("ENROLLMENT_INCOMPLETE_MESSAGE")}</FrontEndTypo.H4>
-      </Alert>
-
-      <CardComponent _body={{ pt: 4 }}>
-        <Pressable onPress={() => navigate(sessionCardProps.navigatePath)}>
-          <HStack alignItems="center" justifyContent="center" space={3}>
-            <Image
-              source={{ uri: "/images/activities/learning-activity.png" }}
-              resizeMode="contain"
-              alignSelf="center"
-              w="75px"
-              h="60px"
-              alt="learning-activity"
-            />
-            <FrontEndTypo.H2 color="textMaroonColor.400">
-              {t(sessionCardProps.type)}
-            </FrontEndTypo.H2>
-          </HStack>
-        </Pressable>
-      </CardComponent>
-    </VStack>
+    <CardComponent _body={{ pt: 4 }}>
+      <Pressable onPress={() => navigate(sessionCardProps.navigatePath)}>
+        <HStack alignItems="center" justifyContent="center" space={3}>
+          <Image
+            source={{ uri: "/images/activities/learning-activity.png" }}
+            resizeMode="contain"
+            alignSelf="center"
+            w="75px"
+            h="60px"
+            alt="learning-activity"
+          />
+          <FrontEndTypo.H2 color="textMaroonColor.400">
+            {t(sessionCardProps.type)}
+          </FrontEndTypo.H2>
+        </HStack>
+      </Pressable>
+    </CardComponent>
   );
 };
