e1daa1b5988c0f1521e34a1da292175f618500f6
snehalsabade <snehal.sabade@tekditechnologies.com>
2024-07-17 12:59:21 +0530

    remove console log

--- apps/front-end/src/pages/front-end/Camp/CampSelectedLearners.js
+++ apps/front-end/src/pages/front-end/Camp/CampSelectedLearners.js
@@ -63,7 +63,6 @@ export default function CampSelectedLearners() {
       setSelectedIds(newSelectedIds);
     }
   };
-  console.log(selectedIds);
   const updateLearner = async () => {
     setIsDisable(true);
     if (selectedIds?.length !== 0) {
