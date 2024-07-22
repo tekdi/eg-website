5f452dc25c5148e38f2e2c1494665a3820819067
Sagar Takle <sagarkoshti1990@gmail.com>
2024-07-22 19:53:44 +0530

    Bug #223179 [WEB UI] : ePCP | Color not getting change as per validations

--- apps/front-end/src/v2/components/Functional/EPCP/EpcpLearnerList.js
+++ apps/front-end/src/v2/components/Functional/EPCP/EpcpLearnerList.js
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { FrontEndTypo, Layout, ObservationService } from "@shiksha/common-lib";
+import {
+  FrontEndTypo,
+  Layout,
+  ObservationService,
+  jsonParse,
+} from "@shiksha/common-lib";
 import { useTranslation } from "react-i18next";
 import { Alert, Avatar, HStack, Pressable, VStack } from "native-base";
 import { useNavigate } from "react-router-dom";
@@ -15,19 +20,23 @@ const EpcpLearnerList = ({ footerLinks, userTokenInfo: { authUser } }) => {
     navigate("/camps");
   };
 
-  const flattenList = (list) => {
+  const flattenList = (list, report) => {
     let flattenedArray = [];
     list?.forEach((item) => {
       item?.group?.group_users?.forEach((userObj) => {
         const { user_id, program_beneficiaries } = userObj?.user;
-        flattenedArray?.push({
-          user_id,
-          first_name: program_beneficiaries?.[0]?.enrollment_first_name,
-          middle_name: program_beneficiaries?.[0]?.enrollment_middle_name,
-          last_name: program_beneficiaries?.[0]?.enrollment_last_name,
-          group_id: item.group.group_id,
-          camp_id: item.camp_id,
-        });
+        const userData = mergingData(
+          {
+            user_id,
+            first_name: program_beneficiaries?.[0]?.enrollment_first_name,
+            middle_name: program_beneficiaries?.[0]?.enrollment_middle_name,
+            last_name: program_beneficiaries?.[0]?.enrollment_last_name,
+            group_id: item.group.group_id,
+            camp_id: item.camp_id,
+          },
+          report
+        );
+        flattenedArray?.push(userData);
       });
     });
 
@@ -39,42 +48,24 @@ const EpcpLearnerList = ({ footerLinks, userTokenInfo: { authUser } }) => {
     const fetchData = async () => {
       let observation = "EPCP";
       const listData = await ObservationService.getCampLearnerList();
-      const flattenedList = flattenList(listData?.data);
       const userIds = listData?.data?.flatMap((group) =>
         group.group.group_users.map((user) => user.user.user_id)
       );
-      const data = await ObservationService.getSubmissionData(
-        userIds,
-        observation
-      );
+      const data = await ObservationService.getSubmissionData({
+        id: userIds,
+        board_id:
+          listData?.data?.[0]?.group?.group_users?.[0]?.user
+            ?.program_beneficiaries?.[0]?.enrolled_for_board,
+
+        observation,
+      });
       const report = data?.data?.[0]?.observation_fields;
-      mergingData(flattenedList, report);
+      const flattenedList = flattenList(listData?.data, report);
+      setLeanerList(flattenedList);
       setLoading(false);
     };
     fetchData();
   }, []);
-
-  const mergingData = (flattenedList, report) => {
-    const mergedArray = flattenedList?.map((user) => {
-      const userData = { ...user };
-      const responses = report?.reduce((acc, observation) => {
-        const fieldResponse = observation.field_responses?.find(
-          (response) => response.context_id === user.user_id
-        );
-        if (fieldResponse) {
-          acc.push({
-            field_id: observation.field_id,
-            response_value: fieldResponse.response_value,
-          });
-        }
-        return acc;
-      }, []);
-      userData.responses = responses;
-      return userData;
-    });
-    const data = ProcessData(mergedArray);
-    setLeanerList(data);
-  };
 
   const ProcessData = (mergedArray) => {
     const newData = mergedArray?.map((user) => {
@@ -82,55 +73,6 @@ const EpcpLearnerList = ({ footerLinks, userTokenInfo: { authUser } }) => {
       return { ...user, status };
     });
     return newData;
-  };
-
-  const getStatus = (responses) => {
-    const response1 = responses?.find((response) => response.field_id === 17);
-    const response2 = responses?.find((response) => response.field_id === 31);
-    const response3 = responses?.find((response) => response.field_id === 22);
-    const response5 = responses?.find((response) => response.field_id === 23);
-    const response7 = responses?.find((response) => response.field_id === 31);
-    // const subjects = JSON.parse(response7?.response_value || "[]");
-    // Safely parse response7's JSON value
-    // let subjects = [];
-    // if (response7?.response_value) {
-    //   try {
-    //     subjects = JSON.parse(response7?.response_value);
-    //   } catch (error) {
-    //     console.error("Error parsing JSON:", error);
-    //   }
-    // }
-
-    const allSelectedYes = responses?.every(
-      (subject) => subject.selected === "yes"
-    );
-    if (!response1 || !response2 || !response3 || !response5 || !response7) {
-      return "not_entered";
-    } else if (
-      response1.response_value === "NO" &&
-      response2 &&
-      response2.response_value !== ""
-    ) {
-      return "not_started";
-    } else if (
-      (response1.response_value === "YES" &&
-        response3.response_value === "NO") ||
-      (response1.response_value === "YES" &&
-        response3.response_value === "YES" &&
-        !allSelectedYes)
-    ) {
-      return "in_progress";
-    } else if (
-      response1.response_value === "YES" &&
-      response3.response_value === "YES" &&
-      allSelectedYes
-    ) {
-      return "completed";
-    } else if (responses.every((response) => response.response_value === "")) {
-      return "not_entered";
-    } else {
-      return "unknown";
-    }
   };
 
   return (
@@ -238,3 +180,64 @@ const EpcpLearnerList = ({ footerLinks, userTokenInfo: { authUser } }) => {
 };
 
 export default EpcpLearnerList;
+
+const mergingData = (userData, report) => {
+  const responses = report?.reduce((acc, observation) => {
+    const fieldResponse = observation?.field_responses?.find(
+      (response) => response.context_id === userData.user_id
+    );
+    if (fieldResponse) {
+      acc?.push({
+        field_id: observation.field_id,
+        field_name: observation.fields?.[0].title,
+        response_value: fieldResponse.response_value,
+      });
+    }
+    return acc;
+  }, []);
+  const status = getStatus(responses);
+  return { ...userData, status };
+};
+
+const getStatus = (responses) => {
+  const HAS_LOGGED_RSOS_APP = responses?.find(
+    (response) => response.field_name === "HAS_LOGGED_RSOS_APP"
+  );
+  const SELECTED_SUBJECT_BY_LEARNER = responses?.find(
+    (response) => response.field_name === "SELECTED_SUBJECT_BY_LEARNER"
+  );
+  const TOOK_EPCP_EXAM_ON_RSOS_APP = responses?.find(
+    (response) => response.field_name === "TOOK_EPCP_EXAM_ON_RSOS_APP"
+  );
+  const subjectData = jsonParse(
+    SELECTED_SUBJECT_BY_LEARNER?.response_value,
+    []
+  );
+  const allSelectedYes = subjectData?.every(
+    (subject) => subject.selected === "yes"
+  );
+
+  if (!HAS_LOGGED_RSOS_APP) {
+    return "not_entered";
+  } else if (HAS_LOGGED_RSOS_APP?.response_value === "NO") {
+    return "not_started";
+  } else if (
+    (HAS_LOGGED_RSOS_APP?.response_value === "YES" &&
+      TOOK_EPCP_EXAM_ON_RSOS_APP?.response_value === "NO") ||
+    (HAS_LOGGED_RSOS_APP?.response_value === "YES" &&
+      TOOK_EPCP_EXAM_ON_RSOS_APP?.response_value === "YES" &&
+      !allSelectedYes)
+  ) {
+    return "in_progress";
+  } else if (
+    HAS_LOGGED_RSOS_APP?.response_value === "YES" &&
+    TOOK_EPCP_EXAM_ON_RSOS_APP?.response_value === "YES" &&
+    allSelectedYes
+  ) {
+    return "completed";
+  } else if (responses.every((response) => response?.response_value === "")) {
+    return "not_entered";
+  } else {
+    return "unknown";
+  }
+};
