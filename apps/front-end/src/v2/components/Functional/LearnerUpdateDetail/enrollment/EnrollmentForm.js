09daf77b9589fb4a29a3d3d319d05b80e19039e2
nawaz shaik <shariefnawaz1995@gmail.com>
2024-08-08 17:39:06 +0530

    Customer Request #224694 Enrollment Details | Add validation for Subjects

--- apps/front-end/src/v2/components/Functional/LearnerUpdateDetail/enrollment/EnrollmentForm.js
+++ apps/front-end/src/v2/components/Functional/LearnerUpdateDetail/enrollment/EnrollmentForm.js
@@ -351,6 +351,38 @@ export default function EnrollmentForm() {
           error = { [key]: t("FUTUTRE_DATES_NOT_ALLOWED") };
         }
         break;
+      case "subjects":
+        if (page === "edit_enrollement_details") {
+          // console.log("subjects", data.subjects, page);
+          const countLanguageSubjects = (subjectsArr, subjects) => {
+            // Convert the subjects array to a Set for faster lookups
+            const subjectsSet = new Set(subjects);
+            // Filter the array for objects with "language" subject_type and a subject_id in the subjects array
+            const languageSubjects = subjectsArr?.filter(
+              (subject) =>
+                subject.subject_type === "language" &&
+                subjectsSet.has(String(subject.subject_id))
+            );
+            // Return the count of filtered objects
+            return languageSubjects.length;
+          };
+          const langCount = countLanguageSubjects(
+            schema?.properties?.subjects?.enumOptions,
+            data?.subjects
+          );
+          const nonLangCount = data?.subjects?.length - langCount;
+          console.log({ langCount, nonLangCount });
+          if (langCount === 0 && nonLangCount === 0) {
+            error = { [key]: t("GROUP_A_GROUP_B_MIN_SUBJECTS") };
+          } else if (langCount > 3 && nonLangCount > 4) {
+            error = { [key]: t("GROUP_A_GROUP_B_MAX_SUBJECTS") };
+          } else if (langCount > 3 && nonLangCount <= 4) {
+            error = { [key]: t("GROUP_A_MAX_SUBJECTS") };
+          } else if (langCount <= 3 && nonLangCount > 4) {
+            error = { [key]: t("GROUP_B_MAX_SUBJECTS") };
+          }
+        }
+        break;
       default:
         break;
     }
