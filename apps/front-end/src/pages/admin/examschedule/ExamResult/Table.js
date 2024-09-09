781317f3f4247ea1f5f512de04093164f35f8d58
sagar takle <sagarkoshti1990@gmail.com>
2024-09-09 17:26:43 +0530

    Customer Request #226586 [WEB ADMIN] : Manual Exam Result Upload - After uploading results manually - filled data is disappearing. Not getting a view result option (#2159)
    * Customer Request #226586 [WEB ADMIN] : Manual Exam Result Upload - After uploading results manually - filled data is disappearing. Not getting a view result option
    
    * fix: remove console log
    
    * fix: filter retain
    
    * fix:
    
    * fix:sonar issue

--- apps/front-end/src/pages/admin/examschedule/ExamResult/Table.js
+++ apps/front-end/src/pages/admin/examschedule/ExamResult/Table.js
@@ -2,22 +2,14 @@ import {
   IconByName,
   AdminTypo,
   tableCustomStyles,
-  enumRegistryService,
   uploadRegistryService,
   ImageView,
 } from "@shiksha/common-lib";
 import { ChipStatus } from "component/BeneficiaryStatus";
 import { ExamChipStatus } from "component/Chip";
-import { HStack, VStack, Pressable, Button, Menu, Modal } from "native-base";
+import { HStack, VStack, Pressable, Menu, Modal } from "native-base";
 
-import React, {
-  memo,
-  useCallback,
-  useState,
-  useMemo,
-  useEffect,
-  useRef,
-} from "react";
+import React, { memo, useCallback, useState, useMemo, useRef } from "react";
 import DataTable from "react-data-table-component";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
@@ -57,7 +49,6 @@ function Table({
   errorMsg,
 }) {
   const { t } = useTranslation();
-  const [selectedData, setSelectedData] = useState();
   const [selectedRow, setSelectedRow] = useState();
   const navigate = useNavigate();
   const uplodInputRef = useRef();
@@ -238,41 +229,13 @@ function Table({
     },
   ];
 
-  useEffect(() => {
-    const getData = async () => {
-      const result = await enumRegistryService.statuswiseCount();
-      setSelectedData(result);
-    };
-    getData();
-  }, []);
-
   const columnsMemoized = useMemo(() => columns(t, navigate), [t, navigate]);
 
   return (
     <VStack>
       <VStack p={2} pt="0">
         <AdminTypo.H5 underline bold color="textMaroonColor.600">
-          {filter?.status === undefined || filter?.status?.length === 0 ? (
-            t("ALL") + `(${paginationTotalRows})`
-          ) : filter?.status?.[0] === "all" ? (
-            <AdminTypo.H4 bold>
-              {t("ALL") + `(${paginationTotalRows})`}
-            </AdminTypo.H4>
-          ) : (
-            filter?.status
-              ?.filter((item) => item)
-              .map(
-                (item) =>
-                  t(item).toLowerCase() +
-                  `(${
-                    selectedData
-                      ? selectedData?.find((e) => item === e.status)?.count
-                      : 0
-                  })`,
-              )
-              .map((item) => item.charAt(0).toUpperCase() + item.slice(1))
-              .join(" , ")
-          )}
+          {t("ALL") + `(${paginationTotalRows})`}
         </AdminTypo.H5>
         <input
           accept="application/pdf"
