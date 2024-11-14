788429df99c9aeb75e8cd953669214e5e46e4b4c
Tushar <tushar.mahajan@tekditechnologies.com>
2024-11-14 17:18:35 +0530

    comment sentry init and breadcrumb

--- lib/common-lib/src/components/AppShell.tsx
+++ lib/common-lib/src/components/AppShell.tsx
@@ -7,18 +7,18 @@ const replaySampleRateString =
   process.env.REACT_APP_SENTRY_REPLAYS_ON_ERROR_SAMPLE_RATE
 
 if (process.env.REACT_APP_SENTRY_ENV) {
-  Sentry.init({
-    environment: `${process.env.REACT_APP_SENTRY_ENV}`,
-    dsn: `${process.env.REACT_APP_SENTRY_DSN_URL}`,
-    integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
-    // Performance Monitoring
-    tracesSampleRate: 0.1, // Capture 100% of the transactions, reduce in production!
-    // Session Replay
-    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: replaySampleRateString
-      ? parseInt(replaySampleRateString)
-      : undefined // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  })
+  // Sentry.init({
+  //   environment: `${process.env.REACT_APP_SENTRY_ENV}`,
+  //   dsn: `${process.env.REACT_APP_SENTRY_DSN_URL}`,
+  //   integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+  //   // Performance Monitoring
+  //   tracesSampleRate: 0.1, // Capture 100% of the transactions, reduce in production!
+  //   // Session Replay
+  //   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  //   replaysOnErrorSampleRate: replaySampleRateString
+  //     ? parseInt(replaySampleRateString)
+  //     : undefined // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  // })
 }
 function AppShell({
   colors,
